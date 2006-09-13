4305c32dca5c7166c73ea844bdcaf69a0ea8df1b
cvsuser <cvsuser>
2006-09-13 17:07:09 +0000

    changing executable bit
    SVN-Revision: 59

---
+++ CVSROOT/log_verify.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import sys
+
+def main(args):
+	tmp = open("/tmp/loginfo.tmp", "w")
+	tmp.write("%s" % '---------------------------------------------------------------')
+	
+	for arg in args:
+		tmp.write("%s\t" % arg)
+		
+	tmp.write('\n')
+	tmp.close()
+	
+	return 0
+
+if __name__ == '__main__':	
+	sys.exit(main(sys.argv[1:]);
