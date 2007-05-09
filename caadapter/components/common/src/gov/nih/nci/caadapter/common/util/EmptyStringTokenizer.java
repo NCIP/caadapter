f702e1056bf3dc5908b278e13a96e7157bb3e998
jayannah <jayannah>
2007-05-09 19:44:17 +0000

    added update method
    SVN-Revision: 115

--- caadapter/components/common/src/gov/nih/nci/caadapter/common/util/EmptyStringTokenizer.java
+++ caadapter/components/common/src/gov/nih/nci/caadapter/common/util/EmptyStringTokenizer.java
@@ -18,134 +18,170 @@
  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. <!-- LICENSE_TEXT_END -->
  */
+
 package gov.nih.nci.caadapter.common.util;
 
 import java.util.ArrayList;
 
 /**
  * This class provides empty tokens if the string has empty commas and toArray() and getTokenAt() implementations.
- * 
+ *
  * @author OWNER: Harsha Jayanna
- * @author LAST UPDATE $Author: wangeug $
- * @version Since caAdapter v3.2 revision $Revision: 1.1 $ date $Date: 2007-04-03 16:02:37 $
+ * @author LAST UPDATE $Author: jayannah $
+ * @version Since caAdapter v3.2 revision $Revision: 1.2 $ date $Date: 2007-05-09 19:44:17 $
  */
 public class EmptyStringTokenizer
 {
-	private ArrayList<String> tokens_;
 
-	private int current_;
+    private ArrayList<String> tokens_;
 
-	public EmptyStringTokenizer(String string, String delimiter) {
-		tokens_ = new ArrayList<String>();
-		current_ = 0;
-		java.util.StringTokenizer tokenizer = new java.util.StringTokenizer( string, delimiter, true);
-		boolean wasDelimiter = true;
-		boolean isDelimiter = false;
-		while (tokenizer.hasMoreTokens()) {
-			String token = tokenizer.nextToken();
-			isDelimiter = token.equals( delimiter);
-			if (wasDelimiter)
-				tokens_.add( isDelimiter ? "" : token);
-			else if (!isDelimiter)
-				tokens_.add( token);
-			wasDelimiter = isDelimiter;
-		}
-		if (isDelimiter)
-			tokens_.add( "");
-	}
+    private int current_;
 
-	public int countTokens()
-	{
-		return tokens_.size();
-	}
+    public EmptyStringTokenizer(String string, String delimiter)
+    {
+        tokens_ = new ArrayList<String>();
+        current_ = 0;
+        java.util.StringTokenizer tokenizer = new java.util.StringTokenizer(string, delimiter, true);
+        boolean wasDelimiter = true;
+        boolean isDelimiter = false;
+        while (tokenizer.hasMoreTokens())
+        {
+            String token = tokenizer.nextToken();
+            isDelimiter = token.equals(delimiter);
+            if (wasDelimiter)
+                tokens_.add(isDelimiter ? "" : token);
+            else if (!isDelimiter)
+                tokens_.add(token);
+            wasDelimiter = isDelimiter;
+        }
+        if (isDelimiter)
+            tokens_.add("");
+    }
 
-	public boolean hasMoreTokens()
-	{
-		return current_ < tokens_.size();
-	}
+    public int countTokens()
+    {
+        return tokens_.size();
+    }
 
-	public boolean hasMoreElements()
-	{
-		return hasMoreTokens();
-	}
+    public boolean hasMoreTokens()
+    {
+        return current_ < tokens_.size();
+    }
 
-	public Object nextElement()
-	{
-		return nextToken();
-	}
+    public boolean hasMoreElements()
+    {
+        return hasMoreTokens();
+    }
 
-	public String nextToken()
-	{
-		String token = (String) tokens_.get( current_);
-		current_++;
-		return token;
-	}
+    public Object nextElement()
+    {
+        return nextToken();
+    }
 
-	public void deleteTokenAt(int _tokenIndex)
-	{
-		tokens_.remove( _tokenIndex);
-	}
+    public String nextToken()
+    {
+        String token = (String) tokens_.get(current_);
+        current_++;
+        return token;
+    }
 
-	public String toString()
-	{
-		StringBuffer _tmpBuf = new StringBuffer();
-		for (int i = 0; i < tokens_.size(); i++) {
-			_tmpBuf.append( tokens_.get( i).toString()+"\\" );
-		}
-		return _tmpBuf.toString();
-	}
-	public String toSameString()
-	{
-		StringBuffer _tmpBuf = new StringBuffer();
-		for (int i = 0; i < tokens_.size(); i++) {
-			_tmpBuf.append( tokens_.get( i).toString()+"," );
-		}
-		return _tmpBuf.toString();
-	}
-	public String toReportString()
-	{
-		StringBuffer _tmpBuf = new StringBuffer();
-		for (int i = 0; i < tokens_.size(); i++) {
-			_tmpBuf.append( tokens_.get( i).toString()+"." );
-		}
-		return _tmpBuf.toString();
-	}
-	
-	public String[] getStringArray(String toBeTokenized, String delims)
-	{
-		EmptyStringTokenizer st = new EmptyStringTokenizer( toBeTokenized, delims);
-		String[] tokenArray = new String[st.countTokens()];
-		int i = 0;
-		while (st.hasMoreTokens()) {
-			tokenArray[i++] = st.nextToken();
-		}
-		return tokenArray;
-	}
+    public void deleteTokenAt(int _tokenIndex)
+    {
+        tokens_.remove(_tokenIndex);
+    }
 
-	public String getTokenAt(int _position)
-	{
-		return tokens_.get( _position);
-	}
+    public String toStringAndRemoveLastSlash()
+    {
+        StringBuffer _tmpBuf = new StringBuffer();
+        for (int i = 0; i < tokens_.size(); i++)
+        {
+            try
+            {
+                tokens_.get(i + 1).toString();
+            } catch (Exception e)
+            {
+                _tmpBuf.append(tokens_.get(i).toString());
+                return _tmpBuf.toString();
+            }
+            _tmpBuf.append(tokens_.get(i).toString() + "\\");
+        }
+        return _tmpBuf.toString();
+    }
 
-	/**
-	 * Testing this class.
-	 * 
-	 * @param args
-	 *            Not used.
-	 */
-	public static void main(String args[])
-	{
-		EmptyStringTokenizer t = new EmptyStringTokenizer( "MSH|^~\\&|_DSMC|MISC^Medical Information Sharing Center|KNUMC|KNUMC^Kyungpook University Medical Center|20030809153902||ADT^A03^ADT_A03|KNU_DSMS20030303194529ADT_AO3-11122211|P^A|2.4|||||KOR|8859/1|^KOREAN", "|");
-		int i = 1;
-		while (t.hasMoreTokens()) {
-			String token = t.nextToken();
-			System.out.println( "times" + i + " - " + token);
-			i++;
-		}
-	}
+    public String toString()
+    {
+        StringBuffer _tmpBuf = new StringBuffer();
+        for (int i = 0; i < tokens_.size(); i++)
+        {
+            _tmpBuf.append(tokens_.get(i).toString() + "\\");
+        }
+        return _tmpBuf.toString();
+    }
+
+    public String toSameString()
+    {
+        StringBuffer _tmpBuf = new StringBuffer();
+        for (int i = 0; i < tokens_.size(); i++)
+        {
+            _tmpBuf.append(tokens_.get(i).toString() + ",");
+        }
+        return _tmpBuf.toString();
+    }
+
+    public String toReportString()
+    {
+        StringBuffer _tmpBuf = new StringBuffer();
+        for (int i = 0; i < tokens_.size(); i++)
+        {
+            _tmpBuf.append(tokens_.get(i).toString() + ".");
+        }
+        return _tmpBuf.toString();
+    }
+
+    public String[] getStringArray(String toBeTokenized, String delims)
+    {
+        EmptyStringTokenizer st = new EmptyStringTokenizer(toBeTokenized, delims);
+        String[] tokenArray = new String[st.countTokens()];
+        int i = 0;
+        while (st.hasMoreTokens())
+        {
+            tokenArray[i++] = st.nextToken();
+        }
+        return tokenArray;
+    }
+
+    public String getTokenAt(int _position)
+    {
+        return tokens_.get(_position);
+    }
+
+    public void updateToken(String token, int position)
+    {
+        tokens_.set(position, token);
+    }
+
+    /**
+     * Testing this class.
+     *
+     * @param args Not used.
+     */
+    public static void main(String args[])
+    {
+        EmptyStringTokenizer t = new EmptyStringTokenizer("MSH|^~\\&|_DSMC|MISC^Medical Information Sharing Center|KNUMC|KNUMC^Kyungpook University Medical Center|20030809153902||ADT^A03^ADT_A03|KNU_DSMS20030303194529ADT_AO3-11122211|P^A|2.4|||||KOR|8859/1|^KOREAN", "|");
+        int i = 1;
+        while (t.hasMoreTokens())
+        {
+            String token = t.nextToken();
+            System.out.println("times" + i + " - " + token);
+            i++;
+        }
+    }
 }
 /**
  * HISTORY : $Log: not supported by cvs2svn $
+ * HISTORY : Revision 1.1  2007/04/03 16:02:37  wangeug
+ * HISTORY : initial loading of common module
+ * HISTORY :
  * HISTORY : Revision 1.5  2006/11/29 18:25:33  jayannah
  * HISTORY : toReportString was missing during check in. Added the method and checked in
  * HISTORY :
