28b074aafda56049db3f01741b46fcb3d4fa7cb6
jayannah <jayannah>
2007-07-18 16:12:16 +0000

    Files for 4.0 release
    SVN-Revision: 289

---
+++ caadapter/components/dataviewer/src/gov/nih/nci/caadapter/dataviewer/helper/TopPaneListenerOpenAction.java
@@ -0,0 +1,60 @@
+package gov.nih.nci.caadapter.dataviewer.helper;
+
+import gov.nih.nci.caadapter.dataviewer.MainDataViewerFrame;
+import gov.nih.nci.caadapter.dataviewer.util.Querypanel;
+import nickyb.sqleonardo.querybuilder.QueryModel;
+import nickyb.sqleonardo.querybuilder.syntax.SQLParser;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hjayanna
+ * Date: May 23, 2007
+ * Time: 10:55:20 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TopPaneListenerOpenAction implements ChangeListener
+{
+
+    private MainDataViewerFrame mD;
+
+    public TopPaneListenerOpenAction(MainDataViewerFrame _mD)
+    {
+        this.mD = _mD;
+    }
+
+    public void stateChanged(ChangeEvent evt)
+    {
+        JTabbedPane pane = (JTabbedPane) evt.getSource();
+        int sel = pane.getSelectedIndex();
+        //System.out.println("jtab title " + pane.getTitleAt(sel));
+        mD.get_jf().repaint();
+        if (!mD.get_alreadyFilled().contains(new Integer(sel)))
+        {
+            /*
+                Get the domain name (meaning the first two characters of the domain name from the hashtable)
+             */
+            try
+            {
+                String queryFromList = mD.getOpenActionQueriesList().get(pane.getTitleAt(sel).substring(0, 2)).toString();
+                QueryModel qm = SQLParser.toQueryModel(queryFromList);
+                int selected = mD.get_tPane().getSelectedIndex();
+                Querypanel panel = (Querypanel) mD.get_aryList().get(selected);
+                panel.get_queryBuilder().setModel(qm);
+                //((Querypanel) mD.get_aryList().get(mD.get_tPane().getSelectedIndex())).get_queryBuilder().setModel(qm);
+                //((Querypanel) mD.get_aryList().get(mD.get_tPane().getSelectedIndex())).get_queryBuilder().getDiagram().getTemporany().callResize();
+                //System.out.println("The number of components are "+((Querypanel) mD.get_aryList().get(mD.get_tPane().getSelectedIndex())).get_queryBuilder().getDiagram().getComponents().length);
+            } catch (Exception e)
+            {
+                e.printStackTrace();//To change body of catch statement use File | Settings | File Templates.
+            }
+            mD.get_alreadyFilled().add(new Integer(mD.get_tPane().getSelectedIndex()));
+            mD.get_lPane().setSelectedIndex(0);
+            mD.get_jf().repaint();
+        }
+        mD.get_lPane().setSelectedIndex(0);
+    }
+}
---
+++ caadapter/components/dataviewer/src/gov/nih/nci/caadapter/dataviewer/util/GetConnectionSingleton.java
@@ -0,0 +1,114 @@
+package gov.nih.nci.caadapter.dataviewer.util;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hjayanna
+ * Date: Jul 9, 2007
+ * Time: 2:02:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GetConnectionSingleton
+{
+
+    private static GetConnectionSingleton singletonObject;
+
+    private static Connection connection;
+
+    private static String serverURL;
+
+    private static String userID;
+
+    private static JPasswordField password;
+
+    public static synchronized Connection getConnection() throws Exception
+    {
+        if (!connection.isClosed())
+            return connection;
+        else
+            return null;
+    }
+
+    public static synchronized String getServerName()
+    {
+        return serverURL;
+    }
+
+    /**
+     * A private Constructor prevents any other class from instantiating.
+     */
+    private GetConnectionSingleton(String drv, String url, String uid, String pwd) throws Exception
+    {
+        try
+        {
+            Class.forName(drv);
+            serverURL = url;
+            userID = uid;
+            connection = DriverManager.getConnection(url, uid, pwd);
+        } catch (Exception e)
+        {
+            throw e;
+        }
+    }
+
+    public static String getPassword()
+    {
+        return password.getText();
+    }
+
+    public static synchronized Connection getConnectionSingletonObject(String drv, String _url, String uid, String pwd) throws Exception
+    {
+        if (singletonObject == null)
+        {
+
+                singletonObject = new GetConnectionSingleton(drv, _url, uid, pwd);
+
+            password = new JPasswordField(pwd);
+        }
+        return connection;
+    }
+
+    public Object clone() throws CloneNotSupportedException
+    {
+        throw new CloneNotSupportedException();
+    }
+
+    public static synchronized void closeConnection()
+    {
+        System.out.println("close connection called");
+        try
+        {
+            if ((connection != null) && (!connection.isClosed()))
+            {
+                connection.close();
+                connection = null;
+                singletonObject = null;
+                serverURL = null;
+                password = null;
+                userID = null;
+            }
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public static String isConnectionAvailable()
+    {
+        try
+        {
+            if ((connection != null) && (!connection.isClosed()))
+            {
+                return serverURL;
+            }
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
