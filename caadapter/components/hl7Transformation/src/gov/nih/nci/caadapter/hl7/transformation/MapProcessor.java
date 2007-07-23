f919b560b6c954525cbd4017e85b7ef07852d8ba
Ye Wu <wuye@mail.nih.gov>
2007-07-23 21:48:47 +0000

    Modified to adapte to the new .map file
    SVN-Revision: 326

--- caadapter/components/hl7Transformation/src/gov/nih/nci/caadapter/hl7/transformation/MapProcessor.java
+++ caadapter/components/hl7Transformation/src/gov/nih/nci/caadapter/hl7/transformation/MapProcessor.java
@@ -37,8 +37,8 @@ import java.util.Set;
  * @author OWNER: Ye Wu
  * @author LAST UPDATE $Author: wuye $
  * @version Since caAdapter v4.0
- *          revision    $Revision: 1.3 $
- *          date        $Date: 2007-07-19 15:11:16 $
+ *          revision    $Revision: 1.4 $
+ *          date        $Date: 2007-07-23 21:48:47 $
  */
 
 public class MapProcessor {
@@ -197,13 +197,78 @@ public class MapProcessor {
     	
     	List<XMLElement> resultList = new ArrayList<XMLElement>();
 
-    	/*
-    	 * TODO come from sibling (like old caadapter)
-    	 */
-    	if (datatype.getCsvSegments().size() > 1) return NullXMLElement.NULL;
+
+    	//If the input are from different siblings
+    	if (datatype.getCsvSegments().size() > 1) {
+//    		List<String> allListNames = new ArrayList<String>();
+    		List<List<CSVSegment>> allListsCSVSegments = new ArrayList<List<CSVSegment>>();
+    		
+    		
+    		for (String csvS:datatype.getCsvSegments()) {
+//    			allListNames.add(csvS);
+    			allListsCSVSegments.add(findCSVSegment(pCsvSegment, csvS));
+    		}
+    		
+    		List <CSVSegment> csvSegmentList = new ArrayList<CSVSegment>();
+    		List <Integer> csvSegmentIndex = new ArrayList<Integer>();
+    		List <Integer> csvSegmentSum = new ArrayList<Integer>();
+    		
+    		int size = allListsCSVSegments.size();
+    		for(int i=0;i<size;i++) {
+    			csvSegmentList.add(allListsCSVSegments.get(i).get(0));
+    			csvSegmentSum.add(allListsCSVSegments.get(i).size());
+    			csvSegmentIndex.add(0);
+    		}
+    		
+    		//--------------
+    		//--------------
+    		boolean hasMore = true;
+    		List<XMLElement> sibXMLElements = new ArrayList<XMLElement>();
+    		while (hasMore) {
+    			sibXMLElements.add(process_datatype_w_sibling(datatype, csvSegmentList, attrName, parentXPath));
+    			for (int i=size-1;i>=0;i--) {
+    				if (csvSegmentIndex.get(i) < csvSegmentSum.get(i)-1) {
+    					hasMore = true;
+    					for (int j = i; j<size; j++) {
+    						int currentIndex = csvSegmentIndex.get(j);
+    						csvSegmentIndex.set(j, currentIndex+1);
+    						csvSegmentList.set(j, allListsCSVSegments.get(j).get(currentIndex+1));
+    					}
+    					break;
+    				}
+    			}
+    		}
+    		return NullXMLElement.NULL;
+    	}
+    	return process_datatype_wo_sibling(datatype, pCsvSegment, attrName, parentXPath);
+    }    	
+    
+
+    private List<XMLElement> process_datatype(Datatype datatype, List<CSVSegment> csvSegments, String attrName, String parentXPath) throws MappingException ,FunctionException {
+//    	System.out.println("Process Datatype:"+datatype.getName()+" attribute name"+attrName);
+    	
+    	if (!datatype.isEnabled()) return NullXMLElement.NULL;
+    	
+    	if (datatype.getCsvSegments() == null)return NullXMLElement.NULL;
+    	
+    	if (datatype.getCsvSegments().size()==0) return NullXMLElement.NULL;
+    	
+    	List<XMLElement> resultList = new ArrayList<XMLElement>();
+
+    	List<XMLElement> returnValue = new ArrayList<XMLElement>();
+    	
+    	returnValue.add(process_datatype_w_sibling(datatype, csvSegments, attrName, parentXPath));
+   		return  returnValue;
+    }
+    	
+    
+    
+    private List<XMLElement> process_datatype_wo_sibling(Datatype datatype, CSVSegment pCsvSegment, String attrName, String parentXPath) throws MappingException ,FunctionException{
+    	
+    	List<XMLElement> resultList = new ArrayList<XMLElement>();
 
     	String current = datatype.getCsvSegments().get(0);
-    	List<CSVSegment> csvSegments = findCSVSegment(pCsvSegment, datatype.getCsvSegments().get(0));
+    	List<CSVSegment> csvSegments = findCSVSegment(pCsvSegment, current);
 
     	for(CSVSegment csvSegment:csvSegments) {
 			XMLElement xmlElement = new XMLElement();
@@ -256,7 +321,69 @@ public class MapProcessor {
     		resultList.add(xmlElement);
     	}   	
     	return resultList;
-    }    	
+    }
+
+    
+    
+    
+    private XMLElement process_datatype_w_sibling(Datatype datatype, List<CSVSegment> csvSegments, String attrName, String parentXPath) throws MappingException ,FunctionException{
+    	
+		XMLElement xmlElement = new XMLElement();
+		xmlElement.setName(attrName);
+
+		Hashtable <String, String> data = new Hashtable<String,String>();
+    	for(CSVSegment csvSegment:csvSegments) {
+    		List<CSVField> csvFields = csvSegment.getFields();
+    		for (CSVField csvField:csvFields) {
+    			data.put(csvField.getXmlPath(),csvField.getValue());
+    		}
+    	}
+		
+		Hashtable<String, Attribute> attrs = datatype.getAttributes();
+		for(String attributeName:(Set<String>)(attrs.keySet())) {
+			Attribute attr = attrs.get(attributeName);
+
+//			String test = parentXPath+"."+attr.getName();
+//			System.out.println(test);
+
+			boolean isSimple = false;
+
+			if (attr.getReferenceDatatype() == null) {
+				isSimple = true;
+			}
+			else {
+				if (attr.getReferenceDatatype().isSimple()) isSimple = true;
+			}
+			if (isSimple) {
+
+//				System.out.println("---------try"+parentXPath+"."+attributeName);
+				String scsXmlPath = mappings.get(parentXPath+"."+attributeName);
+				if (scsXmlPath==null) continue;
+				if (scsXmlPath.startsWith("function.")) { //function mapping to target
+					String datavalue = getFunctionValue(csvSegments,scsXmlPath,data);
+					xmlElement.addAttribute(attributeName, datavalue);
+				}
+				else { //direct mapping from source to target
+//					System.out.println(scsXmlPath);
+					if (data.get(scsXmlPath) == null) { //inverse relationship
+						CSVField csvField = findCSVField(csvSegments, scsXmlPath);
+						xmlElement.addAttribute(attributeName, csvField.getValue());
+					}
+					else {
+						xmlElement.addAttribute(attributeName, data.get(scsXmlPath));
+					}
+				}
+			}
+			else { //complexdatatype
+				List<XMLElement> attrsXMLElement = process_datatype(attr.getReferenceDatatype(), csvSegments,attr.getName(),parentXPath+"."+attr.getName());
+				xmlElement.addChildren(attrsXMLElement);
+			}
+		}
+		return xmlElement;
+    }
+  
+    
+    
     
     private List<CSVSegment> findCSVSegment(CSVSegment csvSegment, String targetXmlPath) {
 //    	System.out.println("CSVSegment "+csvSegment.getXmlPath() + "-->target"+targetXmlPath);
@@ -327,11 +454,40 @@ public class MapProcessor {
     	}
     }
 
+    private CSVField findCSVField(List<CSVSegment> csvSegments, String targetXmlPath) {
+    	
+    	String targetSegmentXmlPath = targetXmlPath.substring(0,targetXmlPath.lastIndexOf('.'));
+    	
+    	for (CSVSegment csvSegment: csvSegments) {
+        	CSVSegment current = csvSegment.getParentSegment();
+    		while (true) {
+    			if (current.getXmlPath().equals(targetSegmentXmlPath)) {
+    				for(CSVField csvField:current.getFields()) {
+    					if (csvField.getXmlPath().equals(targetXmlPath)) return csvField;
+    				}
+    			}
+    			current = current.getParentSegment();
+    			if (current == null) {
+    				break;
+    			}
+    		}
+    	}
+    	
+    	return null;
+    	// Error should be thrown
+    }
+
     public String getFunctionValue(CSVSegment pCsvSegment, String scsXmlPath, Hashtable<String, String> data) throws MappingException ,FunctionException{
+    	List<CSVSegment> csvSegments = new ArrayList<CSVSegment>();
+    	csvSegments.add(pCsvSegment);
+    	return getFunctionValue(csvSegments,scsXmlPath, data);
+    }
+
+    public String getFunctionValue(List<CSVSegment> csvSegments, String scsXmlPath, Hashtable<String, String> data) throws MappingException ,FunctionException{
     	int outputpos = 0;
     	
     	int pos = scsXmlPath.lastIndexOf(".output");
-    	outputpos = Integer.valueOf(scsXmlPath.substring(pos+7, scsXmlPath.length()));
+    	outputpos = Integer.valueOf(scsXmlPath.substring(pos+8, scsXmlPath.length()));
     	String fXmlPath = scsXmlPath.substring(0,pos);
         List<String> inputValues = new ArrayList<String>();
         System.out.println(fXmlPath + " -- " +  outputpos);
@@ -361,13 +517,13 @@ public class MapProcessor {
 //    		if (maps.size() > 1) throw new MappingException("Function must have ONLY one map. " + maps.size() + " found. : " + parameterMeta, null);
 //    		if (maps.size() < 1) throw new MappingException("Function must have one map. Zero found. : " + parameterMeta, null);
 
-    		String inputData = mappings.get("function."+functionComponent.getId()+"."+"input"+i);
+    		String inputData = mappings.get("function."+functionComponent.getId()+"."+"input"+"."+i);
     		if (inputData.startsWith("function.")) { //function mapping to target
-    			inputvalue = getFunctionValue(pCsvSegment,inputData, data);
+    			inputvalue = getFunctionValue(csvSegments,inputData, data);
     		}
     		else { //direct mapping from source to target
     			if (data.get(inputData) == null) { //inverse relationship
-    				CSVField csvField = findCSVField(pCsvSegment, inputData);
+    				CSVField csvField = findCSVField(csvSegments, inputData);
     				inputvalue = csvField.getValue();
     			}
     			else {
@@ -410,6 +566,8 @@ public class MapProcessor {
 
     	return theValue;
     }
+
+
 }
 
 /*
