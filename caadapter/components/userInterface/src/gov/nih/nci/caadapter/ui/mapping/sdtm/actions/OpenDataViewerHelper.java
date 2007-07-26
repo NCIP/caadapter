61c237bc8e8708a3cf9db29eaa9ae59fcd08c6c0
jayannah <jayannah>
2007-07-26 20:05:10 +0000

    Pop up window to specify the map file to load
    SVN-Revision: 346

---
+++ caadapter/components/userInterface/src/gov/nih/nci/caadapter/ui/mapping/sdtm/actions/OpenDataViewerHelper.java
@@ -0,0 +1,27 @@
+package gov.nih.nci.caadapter.ui.mapping.sdtm.actions;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hjayanna
+ * Date: Jul 25, 2007
+ * Time: 3:18:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OpenDataViewerHelper extends JDialog
+{
+
+    OpenDataViewerHelper(Frame owner){
+        JPanel masterPanel = new JPanel();
+        masterPanel.setLayout(new BorderLayout());
+        //
+        JPanel button = new JPanel();
+        button.setLayout(new GridLayout(1,2));
+        //
+
+
+    }
+
+}
