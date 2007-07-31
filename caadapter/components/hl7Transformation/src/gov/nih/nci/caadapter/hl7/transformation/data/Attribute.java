e23b0dd6de0df8fe07b80a8ed844db1e5c44d30f
Ye Wu <wuye@mail.nih.gov>
2007-07-31 14:05:28 +0000

    Attributes of XMLElements
    SVN-Revision: 363

---
+++ caadapter/components/hl7Transformation/src/gov/nih/nci/caadapter/hl7/transformation/data/Attribute.java
@@ -0,0 +1,45 @@
+/**
+ * <!-- LICENSE_TEXT_START -->
+  * <!-- LICENSE_TEXT_END -->
+ */
+
+package gov.nih.nci.caadapter.hl7.transformation.data;
+
+/**
+ * The class contains attributes of a HL7 v3 XMLElement.
+ *
+ * @author OWNER: Ye Wu
+ * @author LAST UPDATE $Author: wuye $
+ * @version Since caAdapter v4.0
+ *          revision    $Revision: 1.1 $
+ *          date        $Date: 2007-07-31 14:05:28 $
+ */
+
+public class Attribute {
+	private String name;
+	private String value;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the value
+	 */
+	public String getValue() {
+		return value;
+	}
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
