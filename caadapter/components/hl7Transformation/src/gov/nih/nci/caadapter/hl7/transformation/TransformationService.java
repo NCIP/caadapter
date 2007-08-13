bb6f8894c79a58b8545bafd304e78b23f9529f7e
Ye Wu <wuye@mail.nih.gov>
2007-08-13 19:21:56 +0000

    load h3s in different format
    SVN-Revision: 498

--- caadapter/components/hl7Transformation/src/gov/nih/nci/caadapter/hl7/transformation/TransformationService.java
+++ caadapter/components/hl7Transformation/src/gov/nih/nci/caadapter/hl7/transformation/TransformationService.java
@@ -15,6 +15,7 @@ import gov.nih.nci.caadapter.common.util.FileUtil;
 import gov.nih.nci.caadapter.common.validation.ValidatorResults;
 import gov.nih.nci.caadapter.hl7.map.FunctionComponent;
 import gov.nih.nci.caadapter.hl7.mif.MIFClass;
+import gov.nih.nci.caadapter.hl7.mif.XmlToMIFImporter;
 import gov.nih.nci.caadapter.hl7.transformation.data.XMLElement;
 import gov.nih.nci.caadapter.hl7.validation.HL7V3MessageValidator;
 
@@ -29,14 +30,14 @@ import java.util.List;
  *
  * @author OWNER: Ye Wu
  * @author LAST UPDATE $Author: wuye $
- * @version $Revision: 1.6 $
- * @date $Date: 2007-08-03 13:25:32 $
+ * @version $Revision: 1.7 $
+ * @date $Date: 2007-08-13 19:21:56 $
  * @since caAdapter v1.2
  */
 
 public class TransformationService
 {
-    public static String RCSID = "$Header: /share/content/gforge/caadapter/caadapter/components/hl7Transformation/src/gov/nih/nci/caadapter/hl7/transformation/TransformationService.java,v 1.6 2007-08-03 13:25:32 wuye Exp $";
+    public static String RCSID = "$Header: /share/content/gforge/caadapter/caadapter/components/hl7Transformation/src/gov/nih/nci/caadapter/hl7/transformation/TransformationService.java,v 1.7 2007-08-13 19:21:56 wuye Exp $";
 
     private boolean isCsvString = false;
     private boolean isInputStream = false;
@@ -188,19 +189,31 @@ public class TransformationService
         
         long loadmifbegintime = System.currentTimeMillis();
         
-        MIFClass mifClass = loadMIF(fullh3sfilepath);
+        MIFClass mifClass = null;
+        if (fullh3sfilepath.endsWith(".h3s"))
+        {
+        	mifClass = loadMIF(fullh3sfilepath);
+        }
+        else 
+        {
+        	XmlToMIFImporter xmlToMIFImporter = new XmlToMIFImporter(); 
+        	mifClass = xmlToMIFImporter.importMifFromXml(new File(fullh3sfilepath));
+        }
         
     	System.out.println("loadmif Parsing time" + (System.currentTimeMillis()-loadmifbegintime));
 
     	MapProcessor mapProcess = new MapProcessor();
         
         List<XMLElement> xmlElements = mapProcess.process(mappings, funcations, csvSegmentedFile, mifClass);
-    	HL7V3MessageValidator validator = new HL7V3MessageValidator();
+//    	HL7V3MessageValidator validator = new HL7V3MessageValidator();
         for(XMLElement xmlElement:xmlElements) 
         {
         	System.out.println("Message:"+xmlElement.toXML());
+        	xmlElement.validate();
         	System.out.println("ValiationResults: " + xmlElement.getValidatorResults().getAllMessages().size());
-        	validator.validate(xmlElement.toXML().toString(), "C:/Projects/caadapter-gforge-2007-May/etc/schemas/multicacheschemas/COCT_MT150003UV03.xsd");
+        	System.out.println("ValiationResults: " + xmlElement.getValidatorResults().getAllMessages());
+        	
+//        	validator.validate(xmlElement.toXML().toString(), "C:/Projects/caadapter-gforge-2007-May/etc/schemas/multicacheschemas/COCT_MT150003UV03.xsd");
         }
         System.out.println("total message" + xmlElements.size());
         return xmlElements;
@@ -279,12 +292,22 @@ public class TransformationService
 //       	TransformationService ts = new TransformationService("C:/xmlpathSpec/xmlpath150003.map",
 //		"C:/xmlpathSpec/COCT_MT150003.csv");
 
-//        TransformationService ts = new TransformationService("D:/projects/caadapter-HEAD/components/hl7Transformation/test/data/COCT_MT150003_MAP1.map",
-//		"D:/projects/caadapter-HEAD/components/hl7Transformation/test/data/COCT_MT150003_MAP_Scenario_Test.csv");
+//        TransformationService ts = new TransformationService(
+//        	"c:/projects/caadapter-gforge-2007-May/components/hl7Transformation/test/data/Transformation/COCT_MT010000_MAP1-1.map",
+//			"c:/projects/caadapter-gforge-2007-May/components/hl7Transformation/test/data/Transformation/COCT_MT01000_Person.csv");
 
-   	TransformationService ts = new TransformationService("C:/xmlpathSpec/NewEncounter_comp.map",
-		"C:/xmlpathSpec/NewEncounter_comp2.csv");
+//        TransformationService ts = new TransformationService(
+//            	"c:/projects/caadapter-gforge-2007-May/components/hl7Transformation/test/data/Transformation/COCT_MT150003_MAP3-1.map",
+//    			"c:/projects/caadapter-gforge-2007-May/components/hl7Transformation/test/data/Transformation/COCT_MT150003_MAP_Scenario_Test_Empty.csv");
+
+        //   	TransformationService ts = new TransformationService("C:/xmlpathSpec/NewEncounter_comp.map",
+//		"C:/xmlpathSpec/NewEncounter_comp2.csv");
         
+   	TransformationService ts = new TransformationService("C:/xmlpathSpec/COCT_MT010000_Simple.map",
+		"C:/xmlpathSpec/COCT_MT010000_Simple.csv");
+
+        //   	TransformationService ts = new TransformationService("C:/xmlpathSpec/010000-Person.map",
+//		"C:/xmlpathSpec/010000-Person.csv");
         ts.process();
        	System.out.println(System.currentTimeMillis()-begintime2);
     }
@@ -292,6 +315,9 @@ public class TransformationService
 
 /**
  * HISTORY      : $Log: not supported by cvs2svn $
+ * HISTORY      : Revision 1.6  2007/08/03 13:25:32  wuye
+ * HISTORY      : Fixed the mapping scenario #1 bug according to the design document
+ * HISTORY      :
  * HISTORY      : Revision 1.5  2007/07/31 14:04:30  wuye
  * HISTORY      : Add Comments
  * HISTORY      :
