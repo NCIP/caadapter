9f88bab31cfcf1aef62c9e5bef83450bdaace5c0
jayannah <jayannah>
2007-08-15 21:16:10 +0000

    Decoupled SQLJAR: Cleaned up the file, disabled the browse button
    SVN-Revision: 523

--- caadapter/components/userInterface/src/gov/nih/nci/caadapter/ui/mapping/sdtm/actions/QBTransformAction.java
+++ caadapter/components/userInterface/src/gov/nih/nci/caadapter/ui/mapping/sdtm/actions/QBTransformAction.java
@@ -3,31 +3,37 @@ package gov.nih.nci.caadapter.ui.mapping.sdtm.actions;
 import gov.nih.nci.caadapter.common.util.EmptyStringTokenizer;
 import gov.nih.nci.caadapter.dataviewer.util.GetConnectionSingleton;
 import gov.nih.nci.caadapter.dataviewer.util.QBParseMappingFile;
+import gov.nih.nci.caadapter.dataviewer.util.QBTransformHelper;
 import gov.nih.nci.caadapter.sdtm.ParseSDTMXMLFile;
 import gov.nih.nci.caadapter.sdtm.SDTMMetadata;
 import gov.nih.nci.caadapter.sdtm.util.CSVMapFileReader;
 import gov.nih.nci.caadapter.ui.common.AbstractMainFrame;
-import gov.nih.nci.caadapter.ui.common.CaadapterFileFilter;
-import gov.nih.nci.caadapter.ui.common.preferences.CaAdapterPref;
 import gov.nih.nci.caadapter.ui.common.tree.DefaultTargetTreeNode;
+import gov.nih.nci.caadapter.ui.main.MainMenuBar;
+import gov.nih.nci.caadapter.ui.mapping.sdtm.DBConnector;
 import gov.nih.nci.caadapter.ui.mapping.sdtm.Database2SDTMMappingPanel;
 import gov.nih.nci.caadapter.ui.mapping.sdtm.RDSFixedLenghtInput;
-import gov.nih.nci.caadapter.ui.main.MainMenuBar;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Iterator;
-import java.util.prefs.Preferences;
 
 /**
  * Created by IntelliJ IDEA.
@@ -36,8 +42,7 @@ import java.util.prefs.Preferences;
  * Time: 2:26:11 PM
  * To change this template use File | Settings | File Templates.
  */
-public class QBTransformAction
-{
+public class QBTransformAction {
 
     JFileChooser directoryLoc, saveXLSLocation;
 
@@ -45,21 +50,25 @@ public class QBTransformAction
 
     private Connection con;
 
-   // Preferences prefs;
+    // Preferences prefs;
 
     HashMap fixedLengthRecords;
 
     boolean fixedLengthIndicator = false;
 
-    public QBTransformAction(AbstractMainFrame _mainFrame, Database2SDTMMappingPanel mappingPanel, Connection _con) throws Exception
-    {
+    Hashtable sqlAsColumnMap;
+
+    /*
+        This constructor is used by the mapping panel
+     */
+    public QBTransformAction(AbstractMainFrame _mainFrame, Database2SDTMMappingPanel mappingPanel, Connection _con) throws Exception {
         //this(_mainFrame, mappingPanel, "");
         this.con = _con;
         directoryLoc = new JFileChooser(System.getProperty("user.dir"));
+        directoryLoc.setDialogTitle("Transforming file " + mappingPanel.getSaveFile().getName() + ", please choose directory..");
         directoryLoc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         int returnVal = directoryLoc.showOpenDialog(_mainFrame);
-        if (((String) MainMenuBar.getCaAdapterPreferences().get("FIXED_LENGTH_VAR")).equalsIgnoreCase("Fixed"))
-        {
+        if (((String) MainMenuBar.getCaAdapterPreferences().get("FIXED_LENGTH_VAR")).equalsIgnoreCase("Fixed")) {
             fixedLengthIndicator = true;
             CSVMapFileReader csvMapFileReader = new CSVMapFileReader(new File(mappingPanel.getSaveFile().getAbsolutePath()));
             //Prepare the list here and keep it ready so that number of blanks corresponding to the
@@ -67,61 +76,55 @@ public class QBTransformAction
             RDSFixedLenghtInput rdsFixedLenghtInput = new RDSFixedLenghtInput(_mainFrame, csvMapFileReader.getTargetKeyList());
             fixedLengthRecords = rdsFixedLenghtInput.getUserValues();
         }
-        if (returnVal == JFileChooser.APPROVE_OPTION)
-        {
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
             directory = directoryLoc.getSelectedFile();
-            try
-            {
+            try {
                 processTransform1(mappingPanel.getSaveFile().getAbsolutePath(), mappingPanel.getDefineXMLLocation(), directory.getAbsolutePath().toString());
-            } catch (Exception e)
-            {
+            } catch (Exception e) {
                 throw e;
-            } finally
-            {
+            } finally {
                 GetConnectionSingleton.closeConnection();
             }
         }
     }
 
-    public QBTransformAction(AbstractMainFrame _mainFrame, Database2SDTMMappingPanel mappingPanel, String newway)
-    {
-        CaadapterFileFilter filter = new CaadapterFileFilter();
-        filter.addExtension("xls");
-        filter.setDescription("xls");
-        saveXLSLocation = new JFileChooser(System.getProperty("user.dir"));
-        saveXLSLocation.setFileFilter(filter);
-        int returnVal = saveXLSLocation.showSaveDialog(_mainFrame);
-        if (returnVal == JFileChooser.APPROVE_OPTION)
-        {
-            String xlsFileName = saveXLSLocation.getSelectedFile().toString();
-            if (!xlsFileName.endsWith(".xls"))
-            {
-                xlsFileName = xlsFileName + ".xls";
-            }
-            System.out.println(xlsFileName);
-            try
-            {
-                processTransform1(mappingPanel.getSaveFile().getAbsolutePath(), mappingPanel.getDefineXMLLocation(), directory.getAbsolutePath().toString());
-            } catch (Exception e)
-            {
-                e.printStackTrace();
+    /*
+        This constructor is used by the menu
+     */
+    public QBTransformAction(AbstractMainFrame _mainFrame, String mapFile, String defineXMLocation, Connection _con) throws Exception {
+        //this(_mainFrame, mappingPanel, "");
+        //process _con since it is always null
+        if(_con==null){
+            String params = getDBParams(mapFile);
+            _con = getConnection(_mainFrame, params, mapFile);
+        }
+        this.con = _con;
+        directoryLoc = new JFileChooser(System.getProperty("user.dir"));
+        directoryLoc.setDialogTitle("Transforming file " + mapFile + ", please choose directory..");
+        directoryLoc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int returnVal = directoryLoc.showOpenDialog(_mainFrame);
+        if (((String) MainMenuBar.getCaAdapterPreferences().get("FIXED_LENGTH_VAR")).equalsIgnoreCase("Fixed")) {
+            fixedLengthIndicator = true;
+            CSVMapFileReader csvMapFileReader = new CSVMapFileReader(new File(mapFile));
+            //Prepare the list here and keep it ready so that number of blanks corresponding to the
+            //value set by the user will be applied appropriately
+            RDSFixedLenghtInput rdsFixedLenghtInput = new RDSFixedLenghtInput(_mainFrame, csvMapFileReader.getTargetKeyList());
+            fixedLengthRecords = rdsFixedLenghtInput.getUserValues();
+        }
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            directory = directoryLoc.getSelectedFile();
+            try {
+                processTransform1(mapFile, defineXMLocation, directory.getAbsolutePath().toString());
+            } catch (Exception e) {
+                throw e;
+            } finally {
+                GetConnectionSingleton.closeConnection();
             }
         }
     }
 
-    public static void main(String args[]) throws Exception
-    {
-        // new QBTransformAction().processTransform1("c:\\d2.map", "c:\\define.xml", "c:\\save.xls");
-    }
 
-    public static Connection getConnection() throws Exception
-    {
-        Class.forName("oracle.jdbc.driver.OracleDriver");
-        return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "hr", "hr");
-    }
-
-    public static Hashtable getAllFieldsForDomains(File SDTMXmlFile)
-    {
+    public static Hashtable getAllFieldsForDomains(File SDTMXmlFile) {
         ParseSDTMXMLFile _parseSDTMFile = new ParseSDTMXMLFile(SDTMXmlFile.getAbsolutePath().toString());
         ArrayList _retArray = _parseSDTMFile.getSDTMStructure();
         Hashtable domainFieldsList = new Hashtable();
@@ -130,18 +133,15 @@ public class QBTransformAction
         ArrayList fieldsString = null;
         String _tempHolder;
         domainFieldsList = new Hashtable();
-        for (int k = 0; k < _retArray.size(); k++)
-        {
-            if (_retArray.get(k).toString().startsWith("KEY"))
-            {
+        for (int k = 0; k < _retArray.size(); k++) {
+            if (_retArray.get(k).toString().startsWith("KEY")) {
                 if (fieldsString != null)
                     domainFieldsList.put(domainString, fieldsString);
                 fieldsString = new ArrayList();
                 EmptyStringTokenizer _str = new EmptyStringTokenizer(_retArray.get(k).toString(), ",");
                 pNode = new DefaultMutableTreeNode(_str.getTokenAt(1).substring(0, 2));
                 domainString = pNode.toString();
-            } else
-            {
+            } else {
                 EmptyStringTokenizer _str = new EmptyStringTokenizer(_retArray.get(k).toString(), ",");
                 _tempHolder = _str.getTokenAt(1);
                 pNode.add(new DefaultTargetTreeNode(new SDTMMetadata(pNode.toString(), _tempHolder, _str.getTokenAt(2), _str.getTokenAt(3), _str.getTokenAt(4))));
@@ -153,15 +153,15 @@ public class QBTransformAction
         return domainFieldsList;
     }
 
-    public void processTransform1(String savedMapFile, String defineXML, String xlsFile) throws Exception
-    {
-        try
-        {
+    public void processTransform1(String savedMapFile, String defineXML, String xlsFile) throws Exception {
+        try {
             QBParseMappingFile qb = new QBParseMappingFile(new File(savedMapFile));
             Hashtable tempTable = getAllFieldsForDomains(new File(defineXML));
+            //prepare the column AS columnname list
+            QBTransformHelper qbTransformHelper = new QBTransformHelper(qb.getHashSQLfromMappings());
+            this.sqlAsColumnMap = qbTransformHelper.getSqlColumnNames();
             Iterator _iter = qb.getHashSQLfromMappings().keySet().iterator();
-            while (_iter.hasNext())
-            {
+            while (_iter.hasNext()) {
                 //get the domain name
                 String domainName = _iter.next().toString();
                 //create a file with domain name
@@ -169,6 +169,7 @@ public class QBTransformAction
                 BufferedWriter out = new BufferedWriter(fstream);
                 //get the query and fire it
                 String query = qb.getHashSQLfromMappings().get(domainName);
+                System.out.println("Query is " + query);
                 ResultSet rs = con.createStatement().executeQuery(query);
                 //get all the columns for the 'domainName'
                 ArrayList columns = qb.getHashTableTransform().get(domainName);
@@ -176,75 +177,125 @@ public class QBTransformAction
                 ArrayList domainHeader = (ArrayList) tempTable.get(domainName);
                 //compute the number of commas for each mapped columnvalue and set if the retrieved value is
                 int rsIncrementer = 1;
-                while (rs.next())
-                {// each row begins here
-                    try
-                    {
+                while (rs.next()) {// each row begins here
+                    try {
                         // if(rsIncrementer == 5000)
                         //     break;
                         int sizeOfEachRow = ((ArrayList) tempTable.get(domainName)).size();
                         ArrayList _tempArray = new ArrayList(sizeOfEachRow + 1);
-                        for (int j = 0; j < (sizeOfEachRow + 1); j++)
-                        {
+                        for (int j = 0; j < (sizeOfEachRow + 1); j++) {
                             _tempArray.add(" ");//add empty buffer
                         }
-                        for (int i = 0; i < columns.size(); i++)
-                        {
+                        for (int i = 0; i < columns.size(); i++) {
                             EmptyStringTokenizer emt = new EmptyStringTokenizer(columns.get(i).toString(), "~");
                             EmptyStringTokenizer getColumn = new EmptyStringTokenizer(emt.nextToken(), ".");
-                            String _dataStr = rs.getString(getColumn.getTokenAt(1) + "_" + getColumn.getTokenAt(2));
+                            String testString = getColumn.getTokenAt(1) + "_" + getColumn.getTokenAt(2);
+                            /*
+                               Read the columnames hashmap and obtain the right 'AS' value to obtain the data from the result set
+                             */
+                            String customColName = fetchCustColNameFromSQLMap(domainName, testString);
+                            String _dataStr = rs.getString(customColName);
                             String empt1 = emt.nextToken().toString();
                             int position = (new Integer(((ArrayList) tempTable.get(domainName)).indexOf(empt1)));
-                            if (fixedLengthRecords!=null && fixedLengthRecords.containsKey(empt1))
-                            {
-                                try
-                                {
+                            if (fixedLengthRecords != null && fixedLengthRecords.containsKey(empt1)) {
+                                try {
                                     _tempArray = implementFixedRec(position, _dataStr, _tempArray, new Integer(fixedLengthRecords.get(empt1).toString()).intValue());
-                                } catch (Exception e)
-                                {
+                                } catch (Exception e) {
                                     String runTimePrp = System.getProperty("debug", "false");
                                     if (new Boolean(runTimePrp))
                                         System.out.println("Problem for target field \"" + empt1 + "\" at position \"" + position + "\" and date was \"" + _dataStr + "\"");
                                 }
-                            } else
-                            {
+                            } else {
                                 _tempArray.remove(position);
-                                try
-                                {
+                                try {
                                     _tempArray.add(position, _dataStr);
-                                } catch (Exception e)
-                                {
+                                } catch (Exception e) {
                                     System.out.println("error at " + position);
                                 }
                             }
                         }
                         out.write("\n" + _tempArray.toString().substring(1, _tempArray.toString().indexOf(']')));
                         rsIncrementer++;
-                    } catch (Exception e)
-                    {
+                    } catch (Exception e) {
                         e.printStackTrace();//To change body of catch statement use File | Settings | File Templates.
                         continue;
                     }
                 }// result set
                 out.close();
             }
-        } catch (SQLException e)
-        {
-            System.out.println(e.getLocalizedMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            //System.out.println(e.getLocalizedMessage());
         }
     }
 
-    private ArrayList implementFixedRec(int position, String srcData, ArrayList _tempArray, int fixedsize) throws Exception
-    {
+    private ArrayList implementFixedRec(int position, String srcData, ArrayList _tempArray, int fixedsize) throws Exception {
         StringBuffer _setSize;
         int finalSize = fixedsize - srcData.length();
         _setSize = new StringBuffer();
         _setSize.append(srcData);
-        for (int i = 0; i < finalSize; i++)
-        {
+        for (int i = 0; i < finalSize; i++) {
             _setSize.append(" ");
         }
         _tempArray.add(position - 1, _setSize.toString());
         return _tempArray;
+    }
+
+    private String fetchCustColNameFromSQLMap(String domainName, String colName) {
+        HashMap tempMap = (HashMap) sqlAsColumnMap.get(domainName);
+        return (String) tempMap.get(colName);
+    }
+
+
+    private Connection getConnection(AbstractMainFrame _mainFrame, String params, String mapFile) {
+        Connection conn = null;
+        QBGetPasswordWindow getPass = new QBGetPasswordWindow(_mainFrame, params, mapFile.toString());
+        String pass = getPass.getPassword();
+        EmptyStringTokenizer empt = new EmptyStringTokenizer(params, "~");
+        try {
+            conn = DBConnector.getDBConnection(empt.getTokenAt(0), empt.getTokenAt(1), empt.getTokenAt(2), pass);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(_mainFrame, e.getMessage().toString()+"\n\n Please restart the application", "Error", JOptionPane.ERROR_MESSAGE);
+            return null;
+        }
+        return conn;
+    }
+
+    private String getDBParams(String mapFileName) {
+
+
+        try {
+            DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+            Document doc = docBuilder.parse(new File(mapFileName));
+            System.out.println("Root element of the doc is " + doc.getDocumentElement().getNodeName());
+            NodeList compLinkNodeList = doc.getElementsByTagName("components");
+            String _xmlFileName = "";
+            for (int s = 0; s < compLinkNodeList.getLength(); s++) {
+                Node node = compLinkNodeList.item(s);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element firstCompElement = (Element) node;
+                    NodeList targetNode = firstCompElement.getElementsByTagName("component");
+                    Element targetName1 = (Element) targetNode.item(0);
+                    targetName1.getAttribute("location").toString();
+                    if (targetName1.getAttribute("kind").toString().equalsIgnoreCase("SCS")) {
+                        // _scsFileName = targetName1.getAttribute("location").toString();
+                    } else if (targetName1.getAttribute("kind").toString().equalsIgnoreCase("Database")) {
+                        return targetName1.getAttribute("param").toString();
+                    }
+                    Element targetName2 = (Element) targetNode.item(1);
+                    if (targetName2.getAttribute("kind").toString().equalsIgnoreCase("XML")) {
+                        _xmlFileName = targetName2.getAttribute("location").toString();
+                    }
+                }
+            }
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (SAXException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return null;
     }
 }
