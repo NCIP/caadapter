b898b5a5a1cf436264ab289292ec1b022a121621
Eugene Wang <wangeug@mail.nih.gov>
2007-08-24 21:15:14 +0000

    add validation level constants
    SVN-Revision: 605

--- caadapter/components/userInterface/src/gov/nih/nci/caadapter/ui/common/preferences/CaAdapterPref.java
+++ caadapter/components/userInterface/src/gov/nih/nci/caadapter/ui/common/preferences/CaAdapterPref.java
@@ -10,15 +10,19 @@ import java.util.HashMap;
  * This class implements preferences in caAdapter
  *
  * @author OWNER: Harsha Jayanna
- * @author LAST UPDATE $Author: jayannah $
+ * @author LAST UPDATE $Author: wangeug $
  * @version Since caAdapter v4.0 revision
- *          $Revision: 1.3 $
- *          $Date: 2007-08-17 15:15:25 $
+ *          $Revision: 1.4 $
+ *          $Date: 2007-08-24 21:15:14 $
  */
 public class CaAdapterPref {
     private static CaAdapterPref caAdapterPref=null;
     private HashMap caAdapterPropHashMap = null;
 
+    public static String VALIDATION_PERFORMANCE_LEVLE_0="level 0";
+    public static String VALIDATION_PERFORMANCE_LEVLE_1="leve 1";
+    public static String VALIDATION_PERFORMANCE_LEVLE_2="level 2";
+    
     private HashMap getcaAdapterPref() {
         return caAdapterPropHashMap;
     }
@@ -56,4 +60,7 @@ public class CaAdapterPref {
 /**
  * Change History
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2007/08/17 15:15:25  jayannah
+ * Reformatted and added the Comments and the log tags for all the files
+ *
  */
