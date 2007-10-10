357846a902625482da676c826e6eda9f51ec84e9
jayannah <jayannah>
2007-10-10 18:45:30 +0000

    Addressed the GUI item changes only
    SVN-Revision: 994

--- caadapter/components/userInterface/src/gov/nih/nci/caadapter/ui/mapping/V2V3/actions/MapV2V3.java
+++ caadapter/components/userInterface/src/gov/nih/nci/caadapter/ui/mapping/V2V3/actions/MapV2V3.java
@@ -1,5 +1,5 @@
 /**
- * <!-- LICENSE_TEXT_START --> $Header: /share/content/gforge/caadapter/caadapter/components/userInterface/src/gov/nih/nci/caadapter/ui/mapping/V2V3/actions/MapV2V3.java,v 1.5 2007-10-03 18:47:03 umkis Exp $
+ * <!-- LICENSE_TEXT_START --> $Header: /share/content/gforge/caadapter/caadapter/components/userInterface/src/gov/nih/nci/caadapter/ui/mapping/V2V3/actions/MapV2V3.java,v 1.6 2007-10-10 18:45:30 jayannah Exp $
  * ****************************************************************** COPYRIGHT NOTICE ****************************************************************** The
  * caAdapter Software License, Version 3.2 Copyright Notice. Copyright 2006 SAIC. This software was developed in conjunction with the National Cancer Institute.
  * To the extent government employees are co-authors, any rights in such works are subject to Title 17 of the United States Code, section 105. Redistribution
@@ -17,7 +17,6 @@
  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. <!-- LICENSE_TEXT_END -->
  */
-
 package gov.nih.nci.caadapter.ui.mapping.V2V3.actions;
 
 import gov.nih.nci.caadapter.common.util.FileUtil;
@@ -37,109 +36,66 @@ import java.io.File;
  * This class provides a SWING panel to help the user to browse the files and choose the necessary files and directories
  *
  * @author OWNER: Harsha Jayanna
- * @author LAST UPDATE $Author: umkis $
- * @version Since caAdapter v3.2 revision $Revision: 1.5 $ date $Date: 2007-10-03 18:47:03 $
+ * @author LAST UPDATE $Author: jayannah $
+ * @version Since caAdapter v3.2 revision $Revision: 1.6 $ date $Date: 2007-10-10 18:45:30 $
  */
 @SuppressWarnings("serial")
-public class MapV2V3 extends JDialog implements ActionListener
-{
-
+public class MapV2V3 extends JDialog implements ActionListener {
     String curDir;
-
     String sourceName1 = "";
-
     String reportName1 = "";
-
     // main is split into north/center regions
     JPanel north = new JPanel(new FlowLayout());
-
     JPanel center = new JPanel(new BorderLayout());
-
     // BorderLayout panel to hold all JPanels using the Center region
     JPanel main = new JPanel(new BorderLayout());
-
     JTextArea log = new JTextArea();
-
     File sourceFile;
-
     File reportFile = new File(reportName1);
-
     // declare file area components
     JLabel sourceLabel1 = new JLabel("Source: ");
-
     JTextField sourceField1 = new JTextField(12);
-
     JButton sourceBrow1 = new JButton("Browse...");
-
     JPanel dirBrowsePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-
     JLabel reportLabel1 = new JLabel("Report: ");
-
     // JLabel reportLabel1 = new JLabel("Report: ");
     JTextField reportField1 = new JTextField(12);
-
     JButton reportBrow1 = new JButton("Browse...");
-
     JPanel hl7MessagePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-
     //JPanel fileZone1 = new JPanel(new GridLayout(6, 3, 2, 2));
     JPanel fileZone1 = new JPanel(new GridLayout(6, 3));
-
     JPanel csvPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-
     JPanel scsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-
     // New fields begin here
     JButton directoryLocation, hl7MessageFilelocation, hl7csvlocation, scsLocation;
-
     JTextField dirLocTextField, hl7MesLocTextField, hl7csvTextField, scsSaveTextField;
-
     JFileChooser directoryLoc, HL7V24Message, saveCSVLocation, scsSaveFileLocation;
-
     JButton process = new JButton("Process");
-
     JButton next = new JButton("Next");
-
     JButton cancel = new JButton("Cancel");
-
     JButton advanced = new JButton("Advanced >>");
-
     // JPanel navZone1 = new JPanel(new GridLayout(3, 1, 2, 2));
     JPanel _bPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-
     JPanel emptyPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-
     File directory, hl7MessageFile, csvSaveFile, scsSaveFile;
-
     String _saveCSV = "", _saveSCS = "";
-
     /*
-      * The SCS file that is generated
-      */ String _genSCSFileName = "";
-
+    * The SCS file that is generated
+    */ String _genSCSFileName = "";
     AbstractMainFrame callingFrame;
 
-    public MapV2V3(AbstractMainFrame _callingFrame)
-    {// start with framework
-        // super("Map V 2.4 to V 3.0 Panel");
+    public MapV2V3(AbstractMainFrame _callingFrame) {
+        new TestWindow(_callingFrame);        
+    }
+    public MapV2V3(AbstractMainFrame _callingFrame, String deprected) {
         callingFrame = _callingFrame;
         this.setModal(true);
         this.setTitle("Map HL7 v2 to HL7 v3 messages");
-        //Color logColor = new Color(220, 220, 220);
-        //setSize(300,275);                                             
-         setLocation(200, 300);
-        //setBounds(325, 325, 710, 275);
-
+        setLocation(200, 300);
         setResizable(false);
-        // setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         sourceField1.setEnabled(false);
         reportField1.setEnabled(false);
-        // sourceField1.setText(sourceName1);
         reportField1.setText(reportName1);
-        //dirBrowsePanel.setBackground(logColor);
-        //hl7MessagePanel.setBackground(logColor);
-        //csvPanel.setBackground(logColor);
-        //scsPanel.setBackground(logColor);
         /**
          * new code begin
          */
@@ -183,7 +139,7 @@ public class MapV2V3 extends JDialog implements ActionListener
         scsPanel.add(scsSaveTextField);
         scsPanel.add(scsLocation);
         //emptyPanel.setBackground(logColor);
-       // _bPanel.setBackground(logColor);
+        // _bPanel.setBackground(logColor);
         emptyPanel.add(new JLabel(""));
         _bPanel.add(advanced);
         advanced.addActionListener(this);
@@ -196,14 +152,6 @@ public class MapV2V3 extends JDialog implements ActionListener
         _bPanel.add(new JLabel("    "));
         _bPanel.add(cancel);
         cancel.addActionListener(this);
-        /**
-         * new code end
-         */
-        //
-        //        dirBrowsePanel.add(dirLabel);
-        //        dirBrowsePanel.add(dirLocTextField);
-        //		  dirBrowsePanel.add(directoryLocation);
-        //        fileZone1.add(dirBrowsePanel);
         fileZone1.add(dirLabel);
         fileZone1.add(dirLocTextField);
         fileZone1.add(directoryLocation);
@@ -233,217 +181,164 @@ public class MapV2V3 extends JDialog implements ActionListener
         fileZone1.add(advanced);
         fileZone1.add(process);
         fileZone1.add(cancel);
-       // fileZone1.setBackground(logColor);
         fileZone1.setBorder(new TitledBorder("File Settings"));
-        // fileZone1.setBorder(new TitledBorder("Command"));
         curDir = System.getProperty("user.dir") + File.separator;
         log.setEnabled(false);
         log.setBorder(new TitledBorder("Log Information"));
-        //log.setBackground(logColor);
-        // center.add("Center", scrollPane);
         main.add("North", fileZone1);
-        // main.add( "Center", log);
         this.getContentPane().add("Center", main);
+        advanced.setPreferredSize(new Dimension(135, 25));
         pack();
         setVisible(true);
     }
 
-    protected ImageIcon createImageIcon(String icon)
-    {
+    protected ImageIcon createImageIcon(String icon) {
         java.net.URL imgURL = getClass().getClassLoader().getResource(icon);
-        if (imgURL != null)
-        {
+        if (imgURL != null) {
             return new ImageIcon(imgURL);
-        } else
-        {
+        } else {
             System.err.println("Couldn't find file: ");
             return null;
         }
     }
 
-    public void actionPerformed(ActionEvent e)
-    {
+    public void actionPerformed(ActionEvent e) {
         String newline = "\n";
-        if (e.getSource() == directoryLocation)
-        {
-            try
-            {
-            } catch (RuntimeException e1)
-            {
+        if (e.getSource() == directoryLocation) {
+            try {
+            } catch (RuntimeException e1) {
                 // TODO Auto-generated catch block
                 e1.printStackTrace();
             }
             int returnVal = directoryLoc.showOpenDialog(MapV2V3.this);
-            if (returnVal == JFileChooser.APPROVE_OPTION)
-            {
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
                 directory = directoryLoc.getSelectedFile();
                 // This is where a real application would open the file.
                 dirLocTextField.setText(directory.getAbsolutePath().toString());
                 dirLocTextField.setEnabled(false);
-            } else
-            {
+            } else {
                 log.append("command cancelled by user." + newline);
             }
             log.setCaretPosition(log.getDocument().getLength());
-        } else if (e.getSource() == hl7MessageFilelocation)
-        {
+        } else if (e.getSource() == hl7MessageFilelocation) {
             int returnVal = HL7V24Message.showOpenDialog(MapV2V3.this);
-            if (returnVal == JFileChooser.APPROVE_OPTION)
-            {
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
                 hl7MessageFile = HL7V24Message.getSelectedFile();
                 hl7MesLocTextField.setText(hl7MessageFile.getAbsolutePath().toString());
                 hl7MesLocTextField.setEnabled(false);
-            } else
-            {
+            } else {
                 log.append("command cancelled by user." + newline);
             }
-        } else if (e.getSource() == hl7csvlocation)
-        {
+        } else if (e.getSource() == hl7csvlocation) {
             CaadapterFileFilter filter = new CaadapterFileFilter();
             filter.addExtension("csv");
             filter.setDescription("csv");
             saveCSVLocation.setFileFilter(filter);
             int returnVal = saveCSVLocation.showSaveDialog(MapV2V3.this);
-            if (returnVal == JFileChooser.APPROVE_OPTION)
-            {
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
                 csvSaveFile = saveCSVLocation.getSelectedFile();
                 // This is where a real application would open the file.
-                if (csvSaveFile.getAbsolutePath().endsWith("csv"))
-                {
+                if (csvSaveFile.getAbsolutePath().endsWith("csv")) {
                     _saveCSV = csvSaveFile.getAbsolutePath();
-                } else
-                {
+                } else {
                     _saveCSV = csvSaveFile.getAbsolutePath() + ".csv";
                 }
                 hl7csvTextField.setText(_saveCSV);
                 hl7csvTextField.setEnabled(false);
-            } else
-            {
+            } else {
                 log.append("command cancelled by user." + newline);
             }
-        } else if (e.getSource() == process)
-        {
-            try
-            {
+        } else if (e.getSource() == process) {
+            try {
                 @SuppressWarnings("unused") String CSVSaveFileName = "";
                 String SCSSaveFileName = "";
-                if (csvSaveFile != null)
-                {
+                if (csvSaveFile != null) {
                     String tmpStr = csvSaveFile.getAbsolutePath().toString();
                     // String tmpStr1 = scsSaveFile.getAbsolutePath().toString();
-                    try
-                    {
-                        if (tmpStr.endsWith("csv"))
-                        {
+                    try {
+                        if (tmpStr.endsWith("csv")) {
                             CSVSaveFileName = csvSaveFile.getAbsolutePath();
-                        } else
-                        {
+                        } else {
                             CSVSaveFileName = csvSaveFile.getAbsolutePath() + ".csv";
                         }
-                    } catch (Exception ee)
-                    {
+                    } catch (Exception ee) {
                         CSVSaveFileName = csvSaveFile.getAbsolutePath() + ".csv";
                     }
-                } else
-                {
+                } else {
                     CSVSaveFileName = hl7csvTextField.getText();
-                    if (!CSVSaveFileName.endsWith("csv"))
-                    {
+                    if (!CSVSaveFileName.endsWith("csv")) {
                         CSVSaveFileName = CSVSaveFileName + ".csv";
                     }
                 }
-                if (scsSaveFile != null)
-                {
+                if (scsSaveFile != null) {
                     String tmpStr = scsSaveFile.getAbsolutePath().toString();
                     // String tmpStr1 = scsSaveFile.getAbsolutePath().toString();
-                    try
-                    {
-                        if (tmpStr.endsWith("scs"))
-                        {
+                    try {
+                        if (tmpStr.endsWith("scs")) {
                             SCSSaveFileName = scsSaveFile.getAbsolutePath();
-                        } else
-                        {
+                        } else {
                             SCSSaveFileName = scsSaveFile.getAbsolutePath() + ".scs";
                         }
-                    } catch (Exception ee)
-                    {
+                    } catch (Exception ee) {
                         SCSSaveFileName = scsSaveFile.getAbsolutePath() + ".scs";
                     }
-                } else
-                {
+                } else {
                     SCSSaveFileName = scsSaveTextField.getText();
-                    if (!SCSSaveFileName.endsWith("scs"))
-                    {
+                    if (!SCSSaveFileName.endsWith("scs")) {
                         SCSSaveFileName = SCSSaveFileName + ".scs";
                     }
                 }
-                try
-                {
-                    if (new gov.nih.nci.caadapter.ui.mapping.V2V3.MappingMain().execute(directory.getAbsolutePath(), hl7MessageFile.getAbsolutePath(), CSVSaveFileName, SCSSaveFileName))
-                    {
+                try {
+                    if (new gov.nih.nci.caadapter.ui.mapping.V2V3.MappingMain().execute(directory.getAbsolutePath(), hl7MessageFile.getAbsolutePath(), CSVSaveFileName, SCSSaveFileName)) {
                         // log.append( "Created the \"" + CSVSaveFileName + "\" successfully" + newline);
                         // log.append( "Created the \"" + SCSSaveFileName + "\" successfully" + newline);
                         this.dispose();
                         JOptionPane.showMessageDialog(callingFrame, "Created the file \"" + CSVSaveFileName + "\" successfully" + newline + "Created the file \"" + SCSSaveFileName + "\" successfully");
-                    } else
-                    {
+                    } else {
                         this.dispose();
                         JOptionPane.showMessageDialog(this, "An error has occured, Please contact Administrator");
                     }
-                } catch (Exception e1)
-                {
+                } catch (Exception e1) {
                     this.dispose();
                     JOptionPane.showMessageDialog(this, "An error has occured, Please contact Administrator " + newline + " Error message is " + e1.getMessage());
                 }
-            } catch (Exception e1)
-            {
+            } catch (Exception e1) {
                 // TODO Auto-generated catch block
                 log.append(e1.getMessage());
             }
-        } else if (e.getSource() == next)
-        {
+        } else if (e.getSource() == next) {
             this.dispose();
             String _sourceFileDir = System.getProperty("user.dir");
             gov.nih.nci.caadapter.ui.mapping.hl7.HL7MappingPanel mp;
-            try
-            {
+            try {
                 mp = new gov.nih.nci.caadapter.ui.mapping.hl7.HL7MappingPanel(_sourceFileDir + "\\" + _genSCSFileName, null);
                 callingFrame.addNewTab(mp);
-            } catch (Exception e1)
-            {
+            } catch (Exception e1) {
                 // TODO Auto-generated catch block
                 e1.printStackTrace();
                 log.append(e1.getMessage());
             }
-        } else if (e.getSource() == cancel)
-        {
+        } else if (e.getSource() == cancel) {
             this.dispose();
-        } else if (e.getSource() == scsLocation)
-        {
+        } else if (e.getSource() == scsLocation) {
             CaadapterFileFilter filter = new CaadapterFileFilter();
             filter.addExtension("scs");
             filter.setDescription("scs");
             scsSaveFileLocation.setFileFilter(filter);
             int returnVal = scsSaveFileLocation.showSaveDialog(MapV2V3.this);
-            if (returnVal == JFileChooser.APPROVE_OPTION)
-            {
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
                 scsSaveFile = scsSaveFileLocation.getSelectedFile();
                 // This is where a real application would open the file.
-                if (scsSaveFile.getAbsolutePath().endsWith("scs"))
-                {
+                if (scsSaveFile.getAbsolutePath().endsWith("scs")) {
                     _saveSCS = scsSaveFile.getAbsolutePath();
-                } else
-                {
+                } else {
                     _saveSCS = scsSaveFile.getAbsolutePath() + ".scs";
                 }
                 scsSaveTextField.setText(_saveSCS);
                 scsSaveTextField.setEnabled(false);
             }
-        } else if (e.getSource() == advanced)
-        {
-            
-            try
-            {
+        } else if (e.getSource() == advanced) {
+            try {
                 this.dispose();
                 V2ConverterToSCSPanel v2ConverterPanel = new V2ConverterToSCSPanel(FileUtil.getV2DataDirPath());
                 JDialog dialog = v2ConverterPanel.setupDialogBasedOnMainFrame(callingFrame);
@@ -452,28 +347,27 @@ public class MapV2V3 extends JDialog implements ActionListener
                 dialog.setSize(v2ConverterPanel.getMinimumSize());
                 dialog.setVisible(true);
                 DefaultSettings.centerWindow(dialog);
-            } catch (Exception e1)
-            {
+            } catch (Exception e1) {
                 // TODO Auto-generated catch block
                 e1.printStackTrace();
             }
         }
     }
 
-    public static void main(String[] args)
-    {
-        try
-        {
+    public static void main(String[] args) {
+        try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
             new MapV2V3(null);
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
 /**
  * HISTORY : $Log: not supported by cvs2svn $
+ * HISTORY : Revision 1.5  2007/10/03 18:47:03  umkis
+ * HISTORY : Protect from crashing and display a fit message when resouce.zip is absent.
+ * HISTORY :
  * HISTORY : Revision 1.4  2007/08/17 01:13:28  umkis
  * HISTORY : generated SCS file using xml path
  * HISTORY :
