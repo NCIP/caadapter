d64c71097702e6c45da6a06db6b611cc4a020b5c
Eugene Wang <wangeug@mail.nih.gov>
2007-10-15 21:29:21 +0000

    clean code
    SVN-Revision: 1050

--- caadapter/components/hl7Transformation/test/src/gov/nih/nci/caadapter/hl7/junit/DemoCSV2HL7v3TransformationTests.java
+++ caadapter/components/hl7Transformation/test/src/gov/nih/nci/caadapter/hl7/junit/DemoCSV2HL7v3TransformationTests.java
@@ -21,7 +21,7 @@ import org.junit.Test;
  * 
  * @author OWNER: Wang, Eugene
  * @author LAST UPDATE $Author: wangeug $
- * @version Since caAdapter v4.0 revision $Revision: 1.3 $ date $Date: 2007-10-15 20:46:26 $
+ * @version Since caAdapter v4.0 revision $Revision: 1.4 $ date $Date: 2007-10-15 21:29:21 $
  */
 
 public class DemoCSV2HL7v3TransformationTests extends TestCase {
@@ -37,22 +37,8 @@ public class DemoCSV2HL7v3TransformationTests extends TestCase {
 		 System.out.println("DemoCSV2HL7v3TransformationTests..mapFie:"+mapFile);
 		 TransformationService ts = new TransformationService(mapFile, csvSrcFile);//
 		 List<XMLElement> xmlElements = ts.process();
-	     assertEquals(1,xmlElements.size());
-	     for(XMLElement rootElement: xmlElements) {
-	        	System.out
-						.println("CSV2HL7v3TransformationTests.testTransformation()..message\n"+rootElement.toXML());
-//	        	rootElement.validate();
-//	        	List<Message> vs = rootElement.getValidatorResults().getAllMessages();
-//	        	assertEquals(9,vs.size());
-//	        	boolean messageFlag = false;
-//	        	for(Message message:vs) {
-//	        		if (message.toString().equals("H3S Data Object modeCode has 2 attribute(s), but HL7 Attribute Encounter.consultant00.modeCode specifies cardinality 0..1"))
-//	        		{
-//	        			messageFlag = true;
-//	        		}
-//	        	}
-//	        	assertEquals(true, messageFlag);
-	        }
+	     for(XMLElement rootElement: xmlElements)
+	        System.out.println("CSV2HL7v3TransformationTests.testTransformation()..message\n"+rootElement.toXML());
 	        
 	 }
 
