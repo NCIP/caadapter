caf1649bdcd6ff6c74b2fdbae8d365c6c2ceaac5
jayannah <jayannah>
2007-10-16 19:52:43 +0000

    This is for testing the database transformation
    SVN-Revision: 1066

---
+++ caadapter/runRDS_DB.bat
@@ -0,0 +1,3 @@
+java -cp .;referenceLib/caAdapter.jar;components/dataviewer/lib/ojdbc14.jar -Ddriver=oracle.jdbc.OracleDriver -Durl=jdbc:oracle:thin:@cbiodb2-d:1521:cadev -Duser=caadapter -Dpass=go!ar987r gov.nih.nci.caadapter.ui.mapping.sdtm.TestDBAPITransform4RDS
+
+
