7f819d430a1e9ce28737565093581e318343974c
Eugene Wang <wangeug@mail.nih.gov>
2007-10-16 19:40:44 +0000

    initial loading
    SVN-Revision: 1064

---
+++ caadapter/demo/apiTest/testHL7Trans.bat
@@ -0,0 +1,8 @@
+rem setup class path
+SET LIB=../lib
+SET EXC_LIB=%LIB%/caAdapter.jar;%LIB%/jdom.jar;%LIB%/castor-0.9.9.jar;%LIB%/xercesImpl.jar;%LIB%/commons-logging-1.0.4.jar;%LIB%/ostermillerutils_1_04_03_for_java_1_4.jar;junit-4.3.1.jar
+
+rem SET FILE_HOME=C:/cvsRepository/cleang/caadapter/workingspace/CSV_to_HL7_V3_Example/150003
+rem SET FILE_HOME=C:/cvsRepository/QARelease/cleanTest/apiTest
+SET FILE_HOME=.
+java -cp ./demoTest.jar;%EXC_LIB% -Dhl7.transformation.src.file=%FILE_HOME%\COCT_MT150003.csv -Dhl7.transformation.map.file=%FILE_HOME%/150003.map junit.textui.TestRunner gov.nih.nci.caadapter.hl7.junit.DemoCSV2HL7v3TransformationTests
