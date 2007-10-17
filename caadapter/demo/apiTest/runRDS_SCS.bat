e59a29734399f0b25e16b72d5042568a0f6c5fcc
jayannah <jayannah>
2007-10-17 14:12:51 +0000

    Test apis for RDS module
    SVN-Revision: 1068

---
+++ caadapter/demo/apiTest/runRDS_SCS.bat
@@ -0,0 +1,3 @@
+java -cp .;../../referenceLib/caAdapter.jar;../../lib/castor-0.9.9.jar;../../components/common/lib/xercesImpl.jar;../../components/common/lib/commons-logging-1.0.4.jar;../../referenceLib/ostermillerutils_1_04_03_for_java_1_4.jar gov.nih.nci.caadapter.hl7.junit.TestCSVAPITransform4RDS
+
+
