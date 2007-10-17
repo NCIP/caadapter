9da649cc45a98923cfcc9fc898482ad928893249
no_author <no_author@no_author.cvs2svn>
2007-10-17 14:43:36 +0000

    This commit was manufactured by cvs2svn to create tag 'caAdapter_4_0_QA_10_17'.
    SVN-Revision: 1072

--- caadapter/components/hl7Transformation/src/gov/nih/nci/caadapter/hl7/transformation/data/NullXMLElement.java
+++
@@ -1,8 +0,0 @@
-package gov.nih.nci.caadapter.hl7.transformation.data;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class NullXMLElement {
-	public static List<XMLElement> NULL = new ArrayList<XMLElement>();
-}
