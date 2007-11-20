32ed38dc887cf36f3a7c971f682025f53f07177c
Eugene Wang <wangeug@mail.nih.gov>
2007-11-20 17:47:53 +0000

    no message
    SVN-Revision: 1211

--- caadapter/demo/apiTest/runRDS_DB.bat
+++
@@ -1,3 +0,0 @@
-java -cp .;demoTestRDS.jar;../../referenceLib/caAdapter.jar;../../components/dataviewer/lib/ojdbc14.jar -Ddriver=oracle.jdbc.OracleDriver -Durl=jdbc:oracle:thin:@cbiodb2-d:1521:cadev -Duser=caadapter -Dpass=go!ar987r gov.nih.nci.caadapter.hl7.junit.TestDBAPITransform4RDS
-
-
