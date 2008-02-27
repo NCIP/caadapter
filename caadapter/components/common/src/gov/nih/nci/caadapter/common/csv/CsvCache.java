278c01b933d496e226fa92575e0811b31ae90f69
Eugene Wang <wangeug@mail.nih.gov>
2008-02-27 14:49:48 +0000

    handle <CR> and <LF>
    SVN-Revision: 1370

--- caadapter/components/common/src/gov/nih/nci/caadapter/common/csv/CsvCache.java
+++ caadapter/components/common/src/gov/nih/nci/caadapter/common/csv/CsvCache.java
@@ -1,6 +1,6 @@
 /**
  * <!-- LICENSE_TEXT_START -->
- * $Header: /share/content/gforge/caadapter/caadapter/components/common/src/gov/nih/nci/caadapter/common/csv/CsvCache.java,v 1.3 2007-11-06 16:31:26 wangeug Exp $
+ * $Header: /share/content/gforge/caadapter/caadapter/components/common/src/gov/nih/nci/caadapter/common/csv/CsvCache.java,v 1.4 2008-02-27 14:49:48 wangeug Exp $
  *
  * ******************************************************************
  * COPYRIGHT NOTICE
@@ -39,15 +39,20 @@ package gov.nih.nci.caadapter.common.csv;
  *
  * @author OWNER: Matthew Giordano
  * @author LAST UPDATE $Author: wangeug $
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 
 //import com.Ostermiller.util.CSVParser;
 
+
+import gov.nih.nci.caadapter.common.util.CaadapterUtil;
+
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -96,27 +101,121 @@ public class CsvCache {
      * @throws IOException
      */
     public static synchronized String[][] getCsvFromInputStream(InputStream dataStream) throws FileNotFoundException, IOException {
-       LineNumberReader lRd=new LineNumberReader(new InputStreamReader(dataStream));
+//    	BufferedReader inBf = new BufferedReader(new InputStreamReader(dataStream));
+//    	ArrayList<String[]> rtnList=new ArrayList<String[]>();
+//    	String lValue="";
+//        String lastValue="";
+//        boolean isComplete=true;
+//    	StringBuffer readBf=new StringBuffer();
+//    	int inChar=inBf.read();
+//
+//    	while (inChar!=-1)
+//    	{
+//    		if (inChar=='\n')
+//    		{
+//    			lValue=readBf.toString();
+//    			readBf=new StringBuffer();
+//		   
+//			   isComplete=isDoubleQuoteClosed(isComplete,lValue);
+//			   if (isComplete)
+//			   {
+//		    	   CsvDataStringParser cParser;
+//		    	   if (lastValue.equals(""))
+//		    		   cParser=new CsvDataStringParser(lValue);
+//		    	   else
+//		    		   cParser=new CsvDataStringParser(lastValue+CaadapterUtil.LINEFEED_ENCODE+lValue);
+//		    	   lastValue="";
+//		    	   rtnList.add(cParser.getDataFields());
+//			   }
+//			   else 
+//			   {
+//				   if (lastValue.equals(""))
+//		    	   {
+//		    		   lastValue=lValue;
+//		    	   }
+//		    	   else
+//		    		   lastValue=lastValue+CaadapterUtil.LINEFEED_ENCODE+lValue;
+//		   		}
+//    		}
+//    		else if (inChar==13)
+//    			readBf.append(CaadapterUtil.CARTRIAGE_RETURN_ENCODE);
+//    		else
+//    			readBf.append(Character.toString((char)inChar));
+//    		inChar=inBf.read();
+//    	}
+    	LineNumberReader lRd=new LineNumberReader(new InputStreamReader(dataStream));
        
        ArrayList<String[]> rtnList=new ArrayList<String[]>();
        String lValue=lRd.readLine();
+       String lastValue="";
+       boolean isComplete=true;
+       
        while (lValue!=null&&!lValue.trim().equals(""))
        {
-    	   CsvDataStringParser cParser=new CsvDataStringParser(lValue);
-    	   rtnList.add(cParser.getDataFields());
+    	   //check if this line is a complete new line
+    	   if (lValue.indexOf("\r")>-1)
+    			   System.out.println("CsvCache.getCsvFromInputStream()..found CR:"+lValue);
+    		   
+    	   isComplete=isDoubleQuoteClosed(isComplete,lValue);
+    	   if (isComplete)
+    	   {
+	    	   CsvDataStringParser cParser;
+	    	   if (lastValue.equals(""))
+	    		   cParser=new CsvDataStringParser(lValue);
+	    	   else
+	    		   cParser=new CsvDataStringParser(lastValue+CaadapterUtil.LINEFEED_ENCODE+lValue);
+	    	   lastValue="";
+	    	   rtnList.add(cParser.getDataFields());
+    	   }
+    	   else 
+    	   {
+    		   if (lastValue.equals(""))
+	    	   {
+	    		   lastValue=lValue;
+	    	   }
+	    	   else
+	    		   lastValue=lastValue+CaadapterUtil.LINEFEED_ENCODE+lValue;
+       		}
     	   lValue=lRd.readLine();
        }
        dataStream.close();
- 
-//       CSVParser csvp = new CSVParser(dataStream);
-//       csvp.getAllValues();
 
        String[][] rtnArray=new String[rtnList.size()][];
        for (int i=0;i<rtnList.size();i++)
     	   rtnArray[i]=rtnList.get(i);
        return  rtnArray;
     }
+    
+    /**
+     * 1. If it is within a double quote, there should be ODD number of double-quote
+     * 2. If it is not within a double quote, there should be EVEN number of double-quote
+     * @param isNewSegment
+     * @param newLineContent
+     * @return
+     */
+    private static boolean isDoubleQuoteClosed(boolean searchNewQuote,String newLineContent )
+    {
+    	if (newLineContent==null||newLineContent.equalsIgnoreCase(""))
+    		return searchNewQuote; 
+    	//there is not any DOUBLE_QUOTE in the string
+    	if (!newLineContent.contains("\""))
+    		return searchNewQuote;
 
+    	for (int i=0;i<newLineContent.length();i++)
+    	{
+    		if (newLineContent.charAt(i)=='\"')
+    			searchNewQuote=!searchNewQuote;
+    	}
+    	return searchNewQuote;
+    }
+    
+    private static String readLineWithCarriageReturn(InputStream dataStream)
+    {
+    	StringBuffer rtnSb=new StringBuffer();
+    	
+    	return rtnSb.toString();
+    	
+    }
 }
 
 
