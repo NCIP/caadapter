65c2216892e0d95622b63d7b49fe7d63b5aec905
Ki Sung Um <umkis@mail.nih.gov>
2008-05-09 16:23:59 +0000

    Go back to 1.5 (08/24/07)
    SVN-Revision: 1460

--- caadapter/components/hl7Transformation/src/gov/nih/nci/caadapter/hl7/mif/MIFReferenceResolver.java
+++ caadapter/components/hl7Transformation/src/gov/nih/nci/caadapter/hl7/mif/MIFReferenceResolver.java
@@ -1,6 +1,5 @@
 package gov.nih.nci.caadapter.hl7.mif;
 import gov.nih.nci.caadapter.common.Log;
-import gov.nih.nci.caadapter.hl7.datatype.DatatypeBaseObject;
 
 import java.util.HashSet;
 import java.util.Hashtable;
@@ -10,9 +9,8 @@ import java.util.TreeSet;
  * @author wangeug
  *
  */
-public class MIFReferenceResolver
-{
-private Hashtable<String, MIFClass> classReferences=new Hashtable<String, MIFClass>();
+public class MIFReferenceResolver {
+private Hashtable<String, MIFClass> classReferences=new Hashtable<String, MIFClass>();;
 private String messageType;
 /**
  * Publically accessed to resolve reference for a MIFClass
@@ -27,18 +25,13 @@ public void getReferenceResolved(MIFClass mifClass)
 }
 /**
  * Resolve reference of a MIFClass recursively
- * If the MIFClass to be resolved is an association MIFClass, 
+ * If the MIFClass to be resolved is an association MIFClass,
  * traversal name should be reset for all choice element class
  * @param mifClass
  */
-private void resolveReference(MIFClass mifClass, Object sender, Hashtable<String, String> asscParticipants)
+private  void resolveReference(MIFClass mifClass, Object sender, Hashtable<String, String>asscParticipants)
 {
-    if (!collectMIFClasses(mifClass))
-    {
-        return;
-    }
-
-    TreeSet<MIFAssociation> mifAsscs=mifClass.getSortedAssociations();
+	TreeSet<MIFAssociation> mifAsscs=mifClass.getSortedAssociations();
 	if (mifAsscs!=null
 		&&!mifAsscs.isEmpty())
 	{
@@ -47,10 +40,10 @@ private void resolveReference(MIFClass mifClass, Object sender, Hashtable<String
 			 MIFClass asscMifClass=assc.getMifClass();
 			 if(asscMifClass.getReferenceName().equals(""))
 			 {
-//##				 classReferences.put(asscMifClass.getName(), asscMifClass);
-//##				 resolveReference(asscMifClass,sender, assc.getParticipantTraversalNames());
+				 classReferences.put(asscMifClass.getName(), asscMifClass);
+				 resolveReference(asscMifClass,sender, assc.getParticipantTraversalNames());
 			 }
-			 else 
+			 else
 			 {
 				//it is a local refereence
 				 MIFClass referedClass=classReferences.get(asscMifClass.getName());
@@ -62,10 +55,10 @@ private void resolveReference(MIFClass mifClass, Object sender, Hashtable<String
 				 }
 				 else
 				 {//put the CMET into classReference for later cloning
-//##					 classReferences.put(asscMifClass.getName(), asscMifClass);
+					 classReferences.put(asscMifClass.getName(), asscMifClass);
 					 Log.logInfo(sender,"Reference is not resolved..className:"+asscMifClass.getName() +"..messageType:"+messageType);
 				 }
-			 }		 
+			 }
 		}
 	}
 	//process choice class
@@ -77,10 +70,10 @@ private void resolveReference(MIFClass mifClass, Object sender, Hashtable<String
 			MIFClass clsToAdd=choiceClass;
 			if(choiceClass.getReferenceName().equals(""))
 			 {
-//##				 classReferences.put(choiceClass.getName(), choiceClass);
-//##				 resolveReference(choiceClass,sender, null);
+				 classReferences.put(choiceClass.getName(), choiceClass);
+				 resolveReference(choiceClass,sender, null);
 			 }
-			else 
+			else
 			 {
 				 MIFClass referedClass=classReferences.get(choiceClass.getName());
 				 if (referedClass!=null) //point the choice class to the reference class
@@ -88,11 +81,7 @@ private void resolveReference(MIFClass mifClass, Object sender, Hashtable<String
 				 else
 				 {
 					 //put the CMET into classReference for later cloning
-
-                     //MIFClass findClass = searchClass(choiceClass.getName());
-
-
-//#                     classReferences.put(choiceClass.getName(), choiceClass);
+					 classReferences.put(choiceClass.getName(), choiceClass);
 					 Log.logError(sender,"Reference is not resolved..className:"+choiceClass.getName() +"..messageType:"+messageType);
 				 }
 			}
@@ -107,102 +96,4 @@ private void resolveReference(MIFClass mifClass, Object sender, Hashtable<String
 		mifClass.setChoice(resolvedChoices);
 	}
 }
-
-    private boolean collectMIFClasses(MIFClass mifClass)
-    {
-        DatatypeBaseObject temp = mifClass;
-        while(true)
-        {
-            if (temp.getParent() == null) break;
-            temp = temp.getParent();
-        }
-        if (!(temp instanceof MIFClass))
-        {
-            System.out.println("This head mif object is not a MIFClass : " + temp.getName() + " : " + mifClass.getName());
-            return false;
-        }
-
-        System.out.println("Head MIFClass : " + temp.getName());
-
-        classReferences=new Hashtable<String, MIFClass>();
-        classReferences.put(temp.getName(), (MIFClass) temp);
-        collectMIFClassesNext((MIFClass) temp);
-        return true;
-    }
-
-    private void collectMIFClassesNext(MIFClass mifClass)
-    {
-        HashSet<MIFClass> choices = mifClass.getChoices();
-
-        Object[] obs = null;
-        if (choices != null)
-        {
-            obs = choices.toArray();
-        }
-
-        if (!((obs == null)||(obs.length == 0)))
-        {
-            for(Object ob:obs)
-            {
-                MIFClass mifOb = (MIFClass) ob;
-                String ref = mifOb.getReferenceName();
-                if ((ref == null)||(ref.trim().equals("")))
-                {
-                    if (checkAndPutMIFClass(mifOb.getName(), mifOb))
-                    {
-                        System.out.println("   MIFClass added (choice) : " + mifOb.getName());
-                        collectMIFClassesNext(mifOb);
-                    }
-                }
-            }
-        }
-
-        HashSet<MIFAssociation> assos = mifClass.getAssociations();
-        obs = null;
-        if (assos != null)
-        {
-            obs = assos.toArray();
-        }
-
-        if (!((obs == null)||(obs.length == 0)))
-        {
-            for(Object ob:obs)
-            {
-                MIFAssociation asso = (MIFAssociation) ob;
-                MIFClass mifOb = asso.getMifClass();
-                String ref = mifOb.getReferenceName();
-                if ((ref == null)||(ref.trim().equals("")))
-                {
-                    if (checkAndPutMIFClass(mifOb.getName(), mifOb))
-                    {
-                        //System.out.println("   MIFClass added (association) : " + mifOb.getName());
-                        collectMIFClassesNext(mifOb);
-                    }
-//                    checkAndPutMIFClass(mifOb.getName(), mifOb);
-//                    System.out.println("   MIFClass added : " + mifOb.getName());
-//                    collectMIFClassesNext(mifOb);
-                }
-            }
-        }
-    }
-
-    private boolean checkAndPutMIFClass(String name, MIFClass mifClass)
-    {
-        MIFClass referedClass=classReferences.get(name);
-	    if (referedClass!=null)
-        {
-            System.out.println(" Duplicate mif class : " + name);
-            return false;
-        }
-        else
-        {
-            classReferences.put(name, mifClass);
-            return true;
-        }
-    }
-
-    public Hashtable<String, MIFClass> getClassReferences()
-    {
-        return classReferences;
-    }
 }
