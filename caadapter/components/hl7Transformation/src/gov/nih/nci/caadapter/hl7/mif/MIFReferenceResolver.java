0a2825f8cc46b3268997816e799fa883bf60d776
Eugene Wang <wangeug@mail.nih.gov>
2008-05-19 20:37:37 +0000

    resolve the class reference only if is not refered to an ancestor class
    SVN-Revision: 1472

--- caadapter/components/hl7Transformation/src/gov/nih/nci/caadapter/hl7/mif/MIFReferenceResolver.java
+++ caadapter/components/hl7Transformation/src/gov/nih/nci/caadapter/hl7/mif/MIFReferenceResolver.java
@@ -3,6 +3,7 @@ import gov.nih.nci.caadapter.common.Log;
 
 import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.LinkedList;
 import java.util.TreeSet;
 /**
  * This class resolve the reference defined within a MIF file
@@ -12,6 +13,7 @@ import java.util.TreeSet;
 public class MIFReferenceResolver {
 private Hashtable<String, MIFClass> classReferences=new Hashtable<String, MIFClass>();;
 private String messageType;
+private LinkedList <String> referencePath=new LinkedList<String>();
 /**
  * Publically accessed to resolve reference for a MIFClass
  * @param mifClass
@@ -21,15 +23,18 @@ public void getReferenceResolved(MIFClass mifClass)
 	//clear the reference hashtable for a new MIFClass
 	classReferences.clear();
 	messageType=mifClass.getMessageType();
+	findReferenceDefinition(mifClass);
+	referencePath.clear();
 	resolveReference(mifClass,this, null);
 }
+
 /**
- * Resolve reference of a MIFClass recursively
- * If the MIFClass to be resolved is an association MIFClass,
- * traversal name should be reset for all choice element class
+ * Find the definition of all the class defined locally
+ * -- local class is defined with an association
+ * -- local class is defined within a choice group
  * @param mifClass
  */
-private  void resolveReference(MIFClass mifClass, Object sender, Hashtable<String, String>asscParticipants)
+private  void findReferenceDefinition(MIFClass mifClass)
 {
 	TreeSet<MIFAssociation> mifAsscs=mifClass.getSortedAssociations();
 	if (mifAsscs!=null
@@ -41,16 +46,53 @@ private  void resolveReference(MIFClass mifClass, Object sender, Hashtable<Strin
 			 if(asscMifClass.getReferenceName().equals(""))
 			 {
 				 classReferences.put(asscMifClass.getName(), asscMifClass);
-				 resolveReference(asscMifClass,sender, assc.getParticipantTraversalNames());
+				 findReferenceDefinition(asscMifClass);
 			 }
-			 else
+
+		}
+	}
+	//process choice class
+	if(mifClass.getSortedChoices()!=null)
+	{
+		for (MIFClass choiceClass:mifClass.getChoices())
+		{
+			if(choiceClass.getReferenceName().equals(""))
+			 {
+				 classReferences.put(choiceClass.getName(), choiceClass);
+				 findReferenceDefinition(choiceClass);
+			 }
+		}
+	}
+}
+/**
+ * Resolve reference of a MIFClass recursively
+ * If the MIFClass to be resolved is an association MIFClass,
+ * traversal name should be reset for all choice element class
+ * @param mifClass
+ */
+private  void resolveReference(MIFClass mifClass, Object sender, Hashtable<String, String>asscParticipants)
+{
+	String clsTodo=mifClass.getName();
+	if (referencePath.contains(clsTodo))
+		return;
+
+	referencePath.addLast(mifClass.getName());
+	TreeSet<MIFAssociation> mifAsscs=mifClass.getSortedAssociations();
+	if (mifAsscs!=null
+		&&!mifAsscs.isEmpty())
+	{
+		for(MIFAssociation assc:mifAsscs)
+		{
+			 MIFClass asscMifClass=assc.getMifClass();
+			 if(asscMifClass.isReference())//.getReferenceName().equals(""))
 			 {
 				//it is a local refereence
 				 MIFClass referedClass=classReferences.get(asscMifClass.getName());
 				 if (referedClass!=null)
 				 {
 					 MIFClass asscRefMifClass=(MIFClass)referedClass.clone();
-//					 resolveReference(asscRefMifClass,sender, assc.getParticipantTraversalNames());
+					 //the resolved class can not be referenced any more
+					 asscRefMifClass.setReference(false);
 					 assc.setMifClass(asscRefMifClass);
 				 }
 				 else
@@ -59,7 +101,11 @@ private  void resolveReference(MIFClass mifClass, Object sender, Hashtable<Strin
 					 Log.logInfo(sender,"Reference is not resolved..className:"+asscMifClass.getName() +"..messageType:"+messageType);
 				 }
 			 }
+			 //recursively resolve the sub-class
+			 resolveReference(assc.getMifClass(),sender, assc.getParticipantTraversalNames());
+
 		}
+		referencePath.removeLast();
 	}
 	//process choice class
 	if(mifClass.getSortedChoices()!=null)
@@ -68,16 +114,15 @@ private  void resolveReference(MIFClass mifClass, Object sender, Hashtable<Strin
 		for (MIFClass choiceClass:mifClass.getChoices())
 		{
 			MIFClass clsToAdd=choiceClass;
-			if(choiceClass.getReferenceName().equals(""))
-			 {
-				 classReferences.put(choiceClass.getName(), choiceClass);
-				 resolveReference(choiceClass,sender, null);
-			 }
-			else
+			if(choiceClass.isReference())//.getReferenceName().equals(""))
 			 {
 				 MIFClass referedClass=classReferences.get(choiceClass.getName());
 				 if (referedClass!=null) //point the choice class to the reference class
+				 {
 					 clsToAdd=(MIFClass)referedClass.clone();
+					 //the resoleved class can not be  referenced any more
+					 clsToAdd.setReference(false);
+				 }
 				 else
 				 {
 					 //put the CMET into classReference for later cloning
@@ -86,6 +131,9 @@ private  void resolveReference(MIFClass mifClass, Object sender, Hashtable<Strin
 				 }
 			}
 			resolvedChoices.add(clsToAdd);
+			 //recursively resolve the sub-class
+			resolveReference(clsToAdd,sender, null);
+			
 			if (asscParticipants!=null)
 			{
 				String resolvedTraversalName=asscParticipants.get(choiceClass.getName());
