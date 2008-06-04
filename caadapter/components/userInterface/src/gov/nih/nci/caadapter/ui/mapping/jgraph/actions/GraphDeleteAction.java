6eee5f8d8b3b56c80ecd12655d10bfc3e7fc7ef5
Eugene Wang <wangeug@mail.nih.gov>
2008-06-04 18:08:06 +0000

    handle delete a table-object dependency mappingl
    SVN-Revision: 1521

--- caadapter/components/userInterface/src/gov/nih/nci/caadapter/ui/mapping/jgraph/actions/GraphDeleteAction.java
+++ caadapter/components/userInterface/src/gov/nih/nci/caadapter/ui/mapping/jgraph/actions/GraphDeleteAction.java
@@ -1,6 +1,6 @@
 /**
  * <!-- LICENSE_TEXT_START -->
- * $Header: /share/content/gforge/caadapter/caadapter/components/userInterface/src/gov/nih/nci/caadapter/ui/mapping/jgraph/actions/GraphDeleteAction.java,v 1.1 2007-04-03 16:17:57 wangeug Exp $
+ * $Header: /share/content/gforge/caadapter/caadapter/components/userInterface/src/gov/nih/nci/caadapter/ui/mapping/jgraph/actions/GraphDeleteAction.java,v 1.2 2008-06-04 18:08:06 wangeug Exp $
  *
  * ******************************************************************
  * COPYRIGHT NOTICE
@@ -34,12 +34,18 @@
 
 package gov.nih.nci.caadapter.ui.mapping.jgraph.actions;
 
+import gov.nih.nci.caadapter.common.SDKMetaData;
+import gov.nih.nci.caadapter.common.metadata.TableMetadata;
+import gov.nih.nci.caadapter.ui.common.jgraph.MappingViewCommonComponent;
 import gov.nih.nci.caadapter.ui.mapping.MappingMiddlePanel;
 import gov.nih.nci.caadapter.ui.mapping.jgraph.MiddlePanelJGraphController;
 
 import org.jgraph.JGraph;
+import org.jgraph.graph.DefaultEdge;
 
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
@@ -49,8 +55,8 @@ import java.awt.event.KeyEvent;
  * @author OWNER: Scott Jiang
  * @author LAST UPDATE $Author: wangeug $
  * @version Since caAdapter v1.2
- *          revision    $Revision: 1.1 $
- *          date        $Date: 2007-04-03 16:17:57 $
+ *          revision    $Revision: 1.2 $
+ *          date        $Date: 2008-06-04 18:08:06 $
  */
 public class GraphDeleteAction extends DefaultAbstractJgraphAction
 {
@@ -113,7 +119,25 @@ public class GraphDeleteAction extends DefaultAbstractJgraphAction
 				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
 			if (userChoice == JOptionPane.YES_OPTION)
 			{
-				getController().handleDelete();
+				Object[] cells = graph.getSelectionCells();
+				DefaultEdge edge = (DefaultEdge) cells[0];
+				MappingViewCommonComponent viewC = (MappingViewCommonComponent) edge.getUserObject();
+				//check if the mapped Table has any mapped column
+				boolean hasMappedColumn=false;
+				DefaultMutableTreeNode tableTreeNode=(DefaultMutableTreeNode) viewC.getSourceNode();
+				for(int i=0;i<tableTreeNode.getChildCount(); i++)
+				{
+					DefaultMutableTreeNode childNode=(DefaultMutableTreeNode)tableTreeNode.getChildAt(0);
+					SDKMetaData columnMeta=(SDKMetaData)childNode.getUserObject();
+					if (columnMeta.isMapped())
+					{
+						hasMappedColumn=true;
+						JOptionPane.showMessageDialog(getMiddlePanel(), "Unable to delete, this mapping has children mapping !", "Children Are Mapped", JOptionPane.WARNING_MESSAGE);
+						break;
+					}					
+				}
+				if (!hasMappedColumn)
+					getController().handleDelete();
 			}
 		}
 		else
@@ -136,6 +160,9 @@ public class GraphDeleteAction extends DefaultAbstractJgraphAction
 }
 /**
  * HISTORY      : $Log: not supported by cvs2svn $
+ * HISTORY      : Revision 1.1  2007/04/03 16:17:57  wangeug
+ * HISTORY      : initial loading
+ * HISTORY      :
  * HISTORY      : Revision 1.10  2006/08/02 18:44:22  jiangsc
  * HISTORY      : License Update
  * HISTORY      :
