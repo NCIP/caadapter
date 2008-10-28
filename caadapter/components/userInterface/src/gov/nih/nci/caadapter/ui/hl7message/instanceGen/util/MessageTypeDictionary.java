7e13049c585621325cd49350f624247214cbe2d7
Ki Sung Um <umkis@mail.nih.gov>
2008-10-28 20:54:11 +0000

    use ClassLoaderTest module
    SVN-Revision: 1741

--- caadapter/components/userInterface/src/gov/nih/nci/caadapter/ui/hl7message/instanceGen/util/MessageTypeDictionary.java
+++ caadapter/components/userInterface/src/gov/nih/nci/caadapter/ui/hl7message/instanceGen/util/MessageTypeDictionary.java
@@ -9,6 +9,7 @@ http://ncicb.nci.nih.gov/infrastructure/cacore_overview/caadapter/indexContent/d
 package gov.nih.nci.caadapter.ui.hl7message.instanceGen.util;
 
 import gov.nih.nci.caadapter.common.ApplicationException;
+import gov.nih.nci.caadapter.common.csv.data.impl.ClassLoaderTest;
 import gov.nih.nci.caadapter.hl7.mif.MIFClass;
 import gov.nih.nci.caadapter.hl7.mif.v1.MIFParserUtil;
 
@@ -16,6 +17,7 @@ import java.net.URL;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.jar.JarFile;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipEntry;
@@ -25,9 +27,9 @@ import java.io.*;
  * This class defines ...
  *
  * @author OWNER: Kisung Um
- * @author LAST UPDATE $Author: wangeug $
+ * @author LAST UPDATE $Author: umkis $
  * @version Since caAdapter v3.3
- *          revision    $Revision: 1.4 $
+ *          revision    $Revision: 1.5 $
  *          date        Aug 3, 2007
  *          Time:       11:33:55 AM $
  */
@@ -46,12 +48,19 @@ public class MessageTypeDictionary
      *
      * @see <a href="http://www.visi.com/~gyles19/cgi-bin/fom.cgi?file=63">JBuilder vice javac serial version UID</a>
      */
-    public static String RCSID = "$Header: /share/content/gforge/caadapter/caadapter/components/userInterface/src/gov/nih/nci/caadapter/ui/hl7message/instanceGen/util/MessageTypeDictionary.java,v 1.4 2008-09-29 20:05:54 wangeug Exp $";
+    public static String RCSID = "$Header: /share/content/gforge/caadapter/caadapter/components/userInterface/src/gov/nih/nci/caadapter/ui/hl7message/instanceGen/util/MessageTypeDictionary.java,v 1.5 2008-10-28 20:54:11 umkis Exp $";
 
-    private String MIF = "mif";
+    private String MIF = "mif.zip";
+    List<String> result = null;
 
-    public MessageTypeDictionary() throws ApplicationException
+    public MessageTypeDictionary(String msgType) throws ApplicationException
     {
+        ClassLoaderTest clt = new ClassLoaderTest(MIF, msgType);
+
+        if (!clt.wasSuccessful()) throw new ApplicationException(clt.getErrormessage());
+
+        result = clt.getListEntryNames();
+        /*
         Enumeration<URL> fileURLs = null;
         try
         {
@@ -71,6 +80,7 @@ public class MessageTypeDictionary
         {
             n++;
             URL fileURL = fileURLs.nextElement();
+            System.out.println("fileURL ("+n+") : " + fileURL);
 
             String url = fileURL.toString();
 
@@ -141,13 +151,28 @@ public class MessageTypeDictionary
             }
 
         }
+        if (n==0) System.err.println("No Elements in this file : " + fileURLs);
+        */
+    }
+    public List<String> getResult()
+    {
+        return result;
     }
 
     public static void main(String[] args)
     {
         try
         {
-            new MessageTypeDictionary();
+            String c = "";
+            if (args.length == 0) c = null;
+            else c = args[0];
+            MessageTypeDictionary mm = new MessageTypeDictionary(c);
+            int n = 0;
+            for(String rst:mm.getResult())
+            {
+                n++;
+                System.out.println("result of searching for '"+c+"' (" + n + ") :" + rst);
+            }
         }
         catch(ApplicationException ae)
         {
@@ -158,6 +183,9 @@ public class MessageTypeDictionary
 
 /**
  * HISTORY      : $Log: not supported by cvs2svn $
+ * HISTORY      : Revision 1.4  2008/09/29 20:05:54  wangeug
+ * HISTORY      : enforce code standard: license file, file description, changing history
+ * HISTORY      :
  * HISTORY      : Revision 1.3  2008/06/09 19:53:53  phadkes
  * HISTORY      : New license text replaced for all .java files.
  * HISTORY      :
