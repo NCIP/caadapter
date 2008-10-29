abf5b8168c1c7027a30e4035b625cdd0743fde26
Eugene Wang <wangeug@mail.nih.gov>
2008-10-29 19:08:50 +0000

    reorder of processing
    SVN-Revision: 1748

--- caadapter/components/hl7Transformation/src/gov/nih/nci/caadapter/hl7/transformation/TransformationObserver.java
+++ caadapter/components/hl7Transformation/src/gov/nih/nci/caadapter/hl7/transformation/TransformationObserver.java
@@ -12,8 +12,8 @@ package gov.nih.nci.caadapter.hl7.transformation;
  * @author OWNER: Eugene Wang
  * @author LAST UPDATE $Author: wangeug $
  * @version Since caAdapter v4.0
- * revision    $Revision: 1.7 $
- * date        $Date: 2008-09-29 15:40:38 $
+ * revision    $Revision: 1.8 $
+ * date        $Date: 2008-10-29 19:08:50 $
  */
 
 public interface TransformationObserver {
@@ -21,13 +21,12 @@ public static int TRANSFORMATION_DATA_LOADING_START=0;
 public static int TRANSFORMATION_DATA_LOADING_READ_MAPPING=1;
 public static int TRANSFORMATION_DATA_LOADING_PARSER_MAPPING=2;
 public static int TRANSFORMATION_DATA_LOADING_READ_H3S_FILE=3;
-public static int TRANSFORMATION_DATA_LOADING_READ_SOURCE=4;
-public static int TRANSFORMATION_DATA_LOADING_PARSER_SOURCE=5;
-public static int TRANSFORMATION_DATA_LOADING_READ_CVS_META=6;
-
+public static int TRANSFORMATION_DATA_LOADING_READ_CVS_META=4;
+public static int TRANSFORMATION_DATA_LOADING_READ_SOURCE=5;
+public static int TRANSFORMATION_DATA_LOADING_PARSER_SOURCE=6;
 public static int TRANSFORMATION_DATA_LOADING_COUNT_MESSAGE=7;
-
 public static int TRANSFORMATION_DATA_LOADING_STEPS=8;
+
 public static String TRANSFORMATION_MESSAGE_GENERATING_STEP="Loading Data ...";
 
 /**
@@ -53,4 +52,7 @@ public void setMessageCount(int count);
 }
 /**
  * HISTORY :$Log: not supported by cvs2svn $
+ * HISTORY :Revision 1.7  2008/09/29 15:40:38  wangeug
+ * HISTORY :enforce code standard: license file, file description, changing history
+ * HISTORY :
  */
