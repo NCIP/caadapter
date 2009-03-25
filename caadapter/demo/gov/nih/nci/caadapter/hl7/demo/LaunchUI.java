44277c81cf5cb27abbfd0b3df4f9729229215cd5
Eugene Wang <wangeug@mail.nih.gov>
2009-03-25 19:56:08 +0000

    VOM integration
    SVN-Revision: 1987

--- caadapter/demo/gov/nih/nci/caadapter/hl7/demo/LaunchUI.java
+++ caadapter/demo/gov/nih/nci/caadapter/hl7/demo/LaunchUI.java
@@ -7,6 +7,7 @@ http://ncicb.nci.nih.gov/infrastructure/cacore_overview/caadapter/indexContent/d
  */
 
 package gov.nih.nci.caadapter.hl7.demo;
+import gov.nih.nci.caadapter.common.util.WebstartUtil;
 import gov.nih.nci.caadapter.ui.main.MainFrame;
 
 import javax.swing.*;
@@ -15,21 +16,26 @@ import javax.swing.*;
  * A tiny driver which will launch the HL7SDK Swing Mapping Tool.
  *
  * @author OWNER: Matthew Giordano
- * @author LAST UPDATE $Author: linc $
- * @version $Revision: 1.10 $
+ * @author LAST UPDATE $Author: wangeug $
+ * @version $Revision: 1.11 $
  * @since caAdapter v1.2
  */
 public class LaunchUI {
     private static final String LOGID = "$RCSfile: LaunchUI.java,v $";
-    public static String RCSID = "$Header: /share/content/gforge/caadapter/caadapter/demo/gov/nih/nci/caadapter/hl7/demo/LaunchUI.java,v 1.10 2008-09-23 15:43:28 linc Exp $";
+    public static String RCSID = "$Header: /share/content/gforge/caadapter/caadapter/demo/gov/nih/nci/caadapter/hl7/demo/LaunchUI.java,v 1.11 2009-03-25 19:56:08 wangeug Exp $";
 
 	public static void main(String[] args)
     {
         try	
             {
+        	System.out.println("LaunchUI.main()..args:"+args);
         		if (args!=null && args.length>0 && args[0].startsWith("http:")) {
+        			System.out.println("LaunchUI.main()..webstart codebase:"+args[0]);
         			gov.nih.nci.caadapter.ui.help.actions.HelpTopicAction.setCodeBase(args[0]);
+        			WebstartUtil.downloadFile("V2_to_V3.vom","workingspace/V2_to_V3.vom");
+        			WebstartUtil.setWebstartDeployed(true);
         		}
+        		
 		        UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
 	        }
             catch (ClassNotFoundException e1)
