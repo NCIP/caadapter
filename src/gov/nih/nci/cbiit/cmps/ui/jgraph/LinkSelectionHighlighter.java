8bbd45d7574e8c2905431ffe8ea4e599e25739fe
Eugene Wang <wangeug@mail.nih.gov>
2009-10-30 14:35:01 +0000

    simplify code: only respond to link highter
    SVN-Revision: 2265

--- src/gov/nih/nci/cbiit/cmps/ui/jgraph/LinkSelectionHighlighter.java
+++ src/gov/nih/nci/cbiit/cmps/ui/jgraph/LinkSelectionHighlighter.java
@@ -11,21 +11,16 @@ package gov.nih.nci.cbiit.cmps.ui.jgraph;
 
 import gov.nih.nci.cbiit.cmps.ui.mapping.CmpsMappingPanel;
 import gov.nih.nci.cbiit.cmps.ui.mapping.MappingMiddlePanel;
-import gov.nih.nci.cbiit.cmps.ui.mapping.MappingTreeScrollPane;
 import gov.nih.nci.cbiit.cmps.ui.properties.DefaultPropertiesSwitchController;
 import gov.nih.nci.cbiit.cmps.ui.tree.MappingSourceTree;
 import gov.nih.nci.cbiit.cmps.ui.tree.MappingTargetTree;
 
-import java.awt.Container;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.EventObject;
 
 import javax.swing.JOptionPane;
-import javax.swing.JPopupMenu;
-import javax.swing.JScrollPane;
 import javax.swing.JTree;
-import javax.swing.SwingUtilities;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
@@ -45,10 +40,10 @@ import org.jgraph.graph.DefaultPort;
  * @author Chunqing Lin
  * @author LAST UPDATE $Author: wangeug $
  * @since     CMPS v1.0
- * @version    $Revision: 1.3 $
- * @date       $Date: 2009-10-28 15:02:44 $
+ * @version    $Revision: 1.4 $
+ * @date       $Date: 2009-10-30 14:35:01 $
  */
-public class LinkSelectionHighlighter extends MouseAdapter implements GraphSelectionListener, TreeSelectionListener
+public class LinkSelectionHighlighter implements GraphSelectionListener
 {
 	/**
 	 * Logging constant used to identify source of log entry, that could be later used to create
@@ -62,55 +57,12 @@ public class LinkSelectionHighlighter extends MouseAdapter implements GraphSelec
 	 *
 	 * @see <a href="http://www.visi.com/~gyles19/cgi-bin/fom.cgi?file=63">JBuilder vice javac serial version UID</a>
 	 */
-	public static String RCSID = "$Header: /share/content/gforge/caadapter/cmps/src/gov/nih/nci/cbiit/cmps/ui/jgraph/LinkSelectionHighlighter.java,v 1.3 2009-10-28 15:02:44 wangeug Exp $";
-
-	private CmpsMappingPanel mappingPanel;
-	private JGraph graph;
-	private MappingMiddlePanel middlePanel;
-	  
-	private boolean graphInSelection = false;
-	private boolean graphInClearSelectionMode = false;
-	private boolean sourceTreeInSelection = false;
-	private boolean targetTreeInSelection = false;
-
-	
-	public LinkSelectionHighlighter(CmpsMappingPanel mappingPanel, JGraph graph, MappingMiddlePanel middlePanel)
+	public static String RCSID = "$Header: /share/content/gforge/caadapter/cmps/src/gov/nih/nci/cbiit/cmps/ui/jgraph/LinkSelectionHighlighter.java,v 1.4 2009-10-30 14:35:01 wangeug Exp $";  
+	private MiddlePanelJGraphController graphController;
+	public LinkSelectionHighlighter(MiddlePanelJGraphController controller)
 	{
-		this.middlePanel = middlePanel;
-		this.mappingPanel = mappingPanel;
-		this.graph = graph;
-		initialize();
+		graphController = controller;
 	}
-
-	private void initialize()
-	{
-		if(mappingPanel!=null)
-		{
-			JTree tree = mappingPanel.getSourceTree();
-			if(tree!=null)
-			{
-				tree.removeMouseListener(this);
-				tree.addMouseListener(this);
-			}
-			tree = mappingPanel.getTargetTree();
-			if(tree!=null)
-			{
-				tree.removeMouseListener(this);
-				tree.addMouseListener(this);
-			}
-		}
-		if(graph!=null)
-		{
-			graph.addMouseListener(this);
-		}
-	}
-
-
-//	void setGraph(JGraph newGraph)
-//	{
-//		graph = newGraph;
-//	}
-
 
 	/**
 	 * Called whenever the value of the selection changes.
@@ -120,96 +72,47 @@ public class LinkSelectionHighlighter extends MouseAdapter implements GraphSelec
 	public void valueChanged(GraphSelectionEvent e)
 	{
 //		Log.logInfo(this, "A new Graph Cell is selected. '" + (e == null ? e : e.getCell()) + "'");
-        try
+		CmpsMappingPanel mappingPanel= graphController.getMappingPanel();
+		try
         {
             if (mappingPanel.isInDragDropMode())
-		    {//if in dragging mode, ignore.
 			    return;
-		    }
         }
         catch(NullPointerException ne)
         {
             JOptionPane.showMessageDialog(mappingPanel, "You should input the source and target file names first(2).", "No Source or Target file", JOptionPane.ERROR_MESSAGE);
             return;
         }
-
-        if(graphInSelection)
-		{
-//			Log.logInfo(this, "In graph selection mode, just a re-entry, so ignore.");
-			return;
-		}
-		//clear tree selections if and only if the call is NOT orignated from any tree node selection
-		if(sourceTreeInSelection)
-		{//clear the opposite
-			mappingPanel.getTargetTree().clearSelection();
-		}
-
-		if(targetTreeInSelection)
-		{//clear the opposite
-			mappingPanel.getSourceTree().clearSelection();
-		}
-
-		graphInSelection = true;
-
-		if(!sourceTreeInSelection && !targetTreeInSelection)
-		{
-            if (mappingPanel.getTargetTree() == null)
-            {
-                JOptionPane.showMessageDialog(mappingPanel, "You should input the source file name first(2).", "No Source file", JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-            else mappingPanel.getTargetTree().clearSelection();
-            if (mappingPanel.getSourceTree() == null)
-            {
-                JOptionPane.showMessageDialog(mappingPanel, "You should input the target file name first(2).", "No Target file", JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-            else mappingPanel.getSourceTree().clearSelection();
-
-            //mappingPanel.getTargetTree().clearSelection();
-			//mappingPanel.getSourceTree().clearSelection();
-		}
-		if(!isAClearSelectionEvent(e))
+        mappingPanel.getMiddlePanelJGraphController().setGraphSelected(true);
+		//the graph is in selection mode, do not set property pan for Element or Attribute
+        if(!isAClearSelectionEvent(e))
 		{//ignore if it is a clear selection event
 			Object obj = e.getCell();
-			if (!graphInClearSelectionMode && obj instanceof DefaultEdge)
+			if (obj instanceof DefaultEdge)
 			{//only handles edge, when graph is NOT in CLEAR selection mode.
 				DefaultEdge edge = (DefaultEdge) obj;
 				Object source = edge.getSource();
 				Object target = edge.getTarget();
 
-				if(!sourceTreeInSelection)
-				{//manually highlight if and only if it is orignated from graph selection.
-					Object sourceUserObject = getUserObject(source);
-					highlightTreeNodeInTree(mappingPanel.getSourceTree(), sourceUserObject);
-				}
+				//manually highlight if and only if it is orignated from graph selection.
+				Object sourceUserObject = getUserObject(source);
+				highlightTreeNodeInTree(mappingPanel.getSourceTree(), sourceUserObject);
+				
 
-				if(!targetTreeInSelection)
-				{//manually highlight if and only if it is orignated from graph selection.
-					Object targetUserObject = getUserObject(target);
-					highlightTreeNodeInTree(mappingPanel.getTargetTree(), targetUserObject);
-				}
+				//manually highlight if and only if it is orignated from graph selection.
+				Object targetUserObject = getUserObject(target);
+				highlightTreeNodeInTree(mappingPanel.getTargetTree(), targetUserObject);
 			}
 		}
-		graphInSelection = false;
 		
-//      The following codes update the property pane with link information
-//      if (graph.getSelectionCount() == 0)
-//      {
-//			setSelectedItem(null);
-//			ChangeEvent changeEvent = new ChangeEvent(this);
-//			propertiesPage.updateProptiesDisplay(changeEvent);
-//      }
-//		else 
 		if(e.getCell() instanceof DefaultGraphCell)
 		{
 			Object newSelection = ((DefaultGraphCell)e.getCell()).getUserObject();
 			DefaultPropertiesSwitchController propertySwitchConroller =(DefaultPropertiesSwitchController)mappingPanel.getMiddlePanelJGraphController().getPropertiesSwitchController();
 			propertySwitchConroller.setSelectedItem(newSelection);
 		}
-		ChangeEvent changeEvent = new ChangeEvent(this);
-		mappingPanel.getMiddlePanelJGraphController().getPropertiesSwitchController().getPropertiesPage().updateProptiesDisplay(changeEvent);
-
+		mappingPanel.getMiddlePanelJGraphController().getPropertiesSwitchController().getPropertiesPage().updateProptiesDisplay(null);
+		mappingPanel.getMiddlePanelJGraphController().setGraphSelected(false);
 	}
 
 	/**
@@ -223,10 +126,6 @@ public class LinkSelectionHighlighter extends MouseAdapter implements GraphSelec
 		if(event instanceof GraphSelectionEvent)
 		{
 			result = !((GraphSelectionEvent) event).isAddedCell();
-		}
-		else if(event instanceof TreeSelectionEvent)
-		{
-			result = !((TreeSelectionEvent) event).isAddedPath();
 		}
 		return result;
 	}
@@ -254,255 +153,15 @@ public class LinkSelectionHighlighter extends MouseAdapter implements GraphSelec
 		{//screen out possible graph cell but just leave pure tree node to be highlighted
 			DefaultMutableTreeNode treeNode = (DefaultMutableTreeNode) object;			
 			TreePath treePath = new TreePath(treeNode.getPath());
-			
-			//System.out.println( "HighlightTreeNodeInTree: " + treePath.toString() );
-			
-//			tree.scrollPathToVisible(treePath);
 			tree.setSelectionPath(treePath);
 		}
-	}
-
-	/**
-	 * Called whenever the value of the selection changes.
-	 *
-	 * @param e the event that characterizes the change.
-	 */
-	public void valueChanged(TreeSelectionEvent e)
-	{
-		try
-        {
-            if (mappingPanel.isInDragDropMode())
-		    {//if in dragging mode, ignore.
-			    return;
-		    }
-        }
-        catch(NullPointerException ne)
-        {
-            JOptionPane.showMessageDialog(mappingPanel, "You should input the source and target file names first(1).", "No Source or Target file", JOptionPane.ERROR_MESSAGE);
-            return;
-        }
-        if (mappingPanel.isInDragDropMode())
-		{//if in dragging mode, ignore.
-			return;
-		}
-		Object eventSource = e.getSource();
-		TreePath path = e.getPath();
-		Object node = (path==null)? null : path.getLastPathComponent();
-//		Log.logInfo(this, "TreeSelectionSource:'" + (eventSource ==null? "null" : eventSource.getClass().getName()) + "'");
-		
-		//System.out.println( "TreeSelectionSource:'" + (eventSource ==null? "null" : eventSource.getClass().getName()) + "'");
-		
-		if(graphInSelection || sourceTreeInSelection || targetTreeInSelection)
-		{//if graph is in selection, no need to execute further logic; otherwise, we may run into an indefinite loop.
-//			Log.logInfo(this, "In Graph or tree selection mode, so just ignore.");
-			return;
-		}
-		else
-		{
-			if(node==null)
-			{
-				return;
-			}
-
-			String searchMode = null;
-			if(eventSource instanceof MappingSourceTree)
-			{
-//				searchMode = MappingViewCommonComponent.SEARCH_BY_SOURCE_NODE;
-				//notify that tree is selection process.
-				//System.out.println( "sourceTreeInSelection is true");
-				sourceTreeInSelection = true;				
-				mappingPanel.getTargetTree().clearSelection();				
-			}
-			else if(eventSource instanceof MappingTargetTree)
-			{
-//				searchMode = MappingViewCommonComponent.SEARCH_BY_TARGET_NODE;
-				//notify that tree is selection process.
-				targetTreeInSelection = true;
-				mappingPanel.getSourceTree().clearSelection();	
-			}
-
-			graphInClearSelectionMode = true;
-			graph.clearSelection();
-			graphInClearSelectionMode = false;
-
-			if(!isAClearSelectionEvent(e) && searchMode!=null)
-			{//if not a clear selection event and searchMode is not null.
-				MiddlePanelJGraphController dataManager = mappingPanel.getMiddlePanelJGraphController();
-//				List<MappingViewCommonComponent> compList = dataManager.findMappingViewCommonComponentList(node, searchMode);
-//				int size = compList.size();
-//				for(int i=0; i<size; i++)
-//				{
-//					MappingViewCommonComponent comp = compList.get(i);
-//					DefaultEdge linkEdge = comp.getLinkEdge();
-//					if(graph!=null)
-//					{
-//						graph.setSelectionCell(linkEdge);
-//					}
-////					else
-////					{//just highlight the tree nodes.
-////						GraphSelectionEvent event = new GraphSelectionEvent(this, new Object[]{linkEdge}, new boolean[]{false});
-////						valueChanged(event);
-////					}
-//
-////					//highlight the other tree node correspondingly
-////					JTree treeToBeHighlighted = null;
-////					DefaultMutableTreeNode treeNodeToBeHighlighted = null;
-////					if(searchMode == MappingViewCommonComponent.SEARCH_BY_TARGET_NODE)
-////					{//to highlight source tree
-////						treeToBeHighlighted = mappingPanel.getSourceTree();
-////					}
-//				}
-			}//end of if(searchMode!=null)
-			if (eventSource instanceof MappingSourceTree)
-			{
-				//notify that tree is end of selection process.
-				sourceTreeInSelection = false;
-			}
-			else if (eventSource instanceof MappingTargetTree)
-			{
-				//notify that tree is end of selection process.
-				targetTreeInSelection = false;
-			}
-		}
-	}
-
-	/**
-	 * Invoked when the mouse has been clicked on a component.
-	 */
-	public void mouseClicked(MouseEvent e)
-	{
-		// If Right Mouse Button
-		if (SwingUtilities.isRightMouseButton(e))
-		{         						
-			Container parentC = e.getComponent().getParent();
-			
-			while ( !(parentC instanceof JScrollPane))
-			{
-				parentC=parentC.getParent();
-			}
-			
-			MappingTreeScrollPane mappingScroll=(MappingTreeScrollPane)parentC;
-			
-			if(mappingScroll.getPaneType().equals(MappingTreeScrollPane.DRAW_NODE_TO_LEFT)) 
-			{
-				// Create PopupMenu for the Cell
-				JPopupMenu menu = createTargetPopupMenu();
-						
-				// Display PopupMenu
-				menu.show(e.getComponent(), e.getX(), e.getY());
-			}
-			
-			if(mappingScroll.getPaneType().equals(MappingTreeScrollPane.DRAW_NODE_TO_RIGHT))
-			{
-				// Create PopupMenu for the Cell
-				JPopupMenu menu = createSourcePopupMenu();
-						
-				// Display PopupMenu
-				menu.show(e.getComponent(), e.getX(), e.getY());
-			}	
-		}
-
-		//if mouse clicked, it is definitely not in drag and drop.
-        boolean previousValue;
-        try
-        {
-            previousValue = mappingPanel.isInDragDropMode();
-        }
-        catch(NullPointerException ne)
-        {
-            JOptionPane.showMessageDialog(mappingPanel, "You should input the source and target file names first.", "No Source or Target file", JOptionPane.WARNING_MESSAGE);
-            return;
-        }
-        
-        if(previousValue)
-		{//previously in drag and drop mode, so to ensure the highlight back up, generate the corresponding tree or graph selection event.
-			Object source = e.getSource();
-			//following code tries to trigger the valueChanged() methods above to mimic and restore the "highlight" command
-			if(source instanceof JGraph)
-			{
-				JGraph mGraph = (JGraph) source;			
-				mGraph.setSelectionCells(mGraph.getSelectionCells());
-			}
-			else if(source instanceof JTree)
-			{
-				JTree mTree = (JTree) source;
-				//following code does not trigger valueChanged(TreeSelectionEvent) above.
-				TreePath[] paths = mTree.getSelectionPaths();
-				int size = paths==null? 0 : paths.length;
-				if(size>0)
-				{
-					boolean[] areNew = new boolean[size];
-					for(int i=0; i<size; i++)
-					{
-						areNew[i] = true;
-					}
-					TreePath leadingPath = mTree.getLeadSelectionPath();
-					TreeSelectionEvent event = new TreeSelectionEvent(source, paths, areNew, null, leadingPath);
-					valueChanged(event);
-				}
-			}
-		}
-	}
-	
-	//
-	// PopupMenu
-	//
-	protected JPopupMenu createSourcePopupMenu()
-	{
-        JTree sourceTree = mappingPanel.getSourceTree();
-        JPopupMenu popupMenu = new JPopupMenu();
-
-        //Primary Key Function
-        String primaryKeyText = "Set as Primary Key";
-
-		return popupMenu;
-	}
-	//
-	// PopupMenu
-	//
-	protected JPopupMenu createTargetPopupMenu()
-	{
-		JPopupMenu popupMenu = new JPopupMenu();
-		
-		String lazyText = "Set as Eager";
-        String clobText = "Set as CLOB";
-        String discriminatorText = "Set as Discrimator";
-
-        //Could change this depending on whether lazy/eager
-	
-		return popupMenu;
-	}
-
-	public String parseNode( String node )
-	{
-		node = replace( node, ", ", "." );
-		node = replace( node, "[", " " );
-		node = replace( node, "]", " " );
-        node = replace( node, "(A)", " " );
-        //node = replace( node, ")", " " );
-        node = replace( node, "Data Model.", "" );
-        node = replace( node, "Object Model.", "" );
-        node = node.trim();        
-		return node; 
-	}
-	
-    static String replace(String str, String pattern, String replace) {
-        int s = 0;
-        int e = 0;
-        StringBuffer result = new StringBuffer();
-    
-        while ((e = str.indexOf(pattern, s)) >= 0) {
-            result.append(str.substring(s, e));
-            result.append(replace);
-            s = e+pattern.length();
-        }
-        result.append(str.substring(s));
-        return result.toString();
-    }
-    
+	}	    
 }
 /**
  * HISTORY: $Log: not supported by cvs2svn $
+ * HISTORY: Revision 1.3  2009/10/28 15:02:44  wangeug
+ * HISTORY: hook property panel with link graph
+ * HISTORY:
  * HISTORY: Revision 1.2  2008/12/04 21:34:20  linc
  * HISTORY: Drap and Drop support with new Swing.
  * HISTORY:
