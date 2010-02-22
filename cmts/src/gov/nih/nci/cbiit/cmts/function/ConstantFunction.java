5ca88e645cfc68a7943c8b88f5e3484ce13a6b23
Eugene Wang <wangeug@mail.nih.gov>
2010-02-22 19:35:14 +0000

    enable functions: temporarily remove dateTime.changeFormat, math.subtraction, math.multiplication, math.division
    SVN-Revision: 2530

--- cmts/src/gov/nih/nci/cbiit/cmts/function/ConstantFunction.java
+++ cmts/src/gov/nih/nci/cbiit/cmts/function/ConstantFunction.java
@@ -5,11 +5,16 @@ import java.util.Map;
 import gov.nih.nci.cbiit.cmts.core.FunctionType;
 
 public class ConstantFunction {
-
-	public String retrieveValue(FunctionType functionType, Map paramters)
+/**
+ * Returns the xml query statement of retrieving a constant value
+ * @param functionType
+ * @param paramters
+ * @return
+ */
+	public String retrieveValue(FunctionType functionType, Map<String, String> paramters)
 	{
 		StringBuffer rtnBf=new StringBuffer();
 		rtnBf.append(functionType.getData().get(0).getValue());
-		return rtnBf.toString();
+		return "string(\""+rtnBf.toString()+"\")";
 	}
 }
