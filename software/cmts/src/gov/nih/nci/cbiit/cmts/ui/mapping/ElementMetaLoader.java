06e3efffbd1d6733a6a2ba778fffa0b3f0ece6e9
Eugene Wang <wangeug@mail.nih.gov>
2010-03-23 18:20:46 +0000

    clean code, work on recursive issues
    SVN-Revision: 2587

--- software/cmts/src/gov/nih/nci/cbiit/cmts/ui/mapping/ElementMetaLoader.java
+++ software/cmts/src/gov/nih/nci/cbiit/cmts/ui/mapping/ElementMetaLoader.java
@@ -9,17 +9,14 @@
 package gov.nih.nci.cbiit.cmts.ui.mapping;
 
 import gov.nih.nci.cbiit.cmts.core.AttributeMeta;
+import gov.nih.nci.cbiit.cmts.core.BaseMeta;
 import gov.nih.nci.cbiit.cmts.core.Component;
 import gov.nih.nci.cbiit.cmts.core.ElementMeta;
 import gov.nih.nci.cbiit.cmts.ui.tree.DefaultMappableTreeNode;
 import gov.nih.nci.cbiit.cmts.ui.tree.DefaultSourceTreeNode;
 import gov.nih.nci.cbiit.cmts.ui.tree.DefaultTargetTreeNode;
 
-import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeModel;
-import javax.swing.tree.MutableTreeNode;
-import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreeNode;
 
 import java.io.Serializable;
@@ -45,127 +42,11 @@ public class ElementMetaLoader implements Serializable
 {
 	public static final int SOURCE_MODE = 0;
 	public static final int TARGET_MODE = 1;
-	int mode = SOURCE_MODE;
-	Object obj = null;
+	private int nodeMode = SOURCE_MODE;
+	private Object nodeRoot = null;
 
 	public ElementMetaLoader(int mode){
-		this.mode = mode;
-	}
-	
-	/**
-	 * To descendant of this class:
-	 * This is an overridable function to allow descendant class to provide
-	 * different tree node implementations.
-	 * @param userObject
-	 * @return a tree node that wraps the user object.
-	 */
-	protected DefaultMutableTreeNode constructTreeNode(Object userObject)
-	{
-		return constructTreeNode(userObject, true);
-	}
-
-	/**
-	 * Overloaded version of the function above.
-	 *
-	 * @param userObject
-	 * @param allowsChildren
-	 * @return a tree node that wraps the user object.
-	 */
-	public DefaultMutableTreeNode constructTreeNode(Object userObject, boolean allowsChildren)
-	{
-		DefaultMutableTreeNode node = null;
-		switch(mode){
-		case SOURCE_MODE:
-			node = new DefaultSourceTreeNode(getUserObject(userObject), allowsChildren);
-			break;
-		case TARGET_MODE:
-			node = new DefaultTargetTreeNode(getUserObject(userObject), allowsChildren);
-			break;
-		default:
-			node = new DefaultMappableTreeNode(getUserObject(userObject), allowsChildren);
-		}
-		return node;
-	}
-
-	public class MyTreeObject implements Serializable {
-		String name;
-		Object obj;
-		Object rootObj;
-		
-		public MyTreeObject(String name, Object obj) {
-			this.rootObj = ElementMetaLoader.this.obj;
-			this.name = name;
-			this.obj = obj;
-		}
-		/**
-		 * @return the name
-		 */
-		public String getName() {
-			return name;
-		}
-		/**
-		 * @param name the name to set
-		 */
-		public void setName(String name) {
-			this.name = name;
-		}
-		/**
-		 * @return the obj
-		 */
-		public Object getObj() {
-			return obj;
-		}
-		/**
-		 * @return the rootObj
-		 */
-		public Object getRootObj() {
-			return rootObj;
-		}
-		
-		public String toString(){
-			return name;
-		}
-		
-	}
-	private Object getUserObject(Object userObject) {
-		if(userObject instanceof ElementMeta){
-			return new MyTreeObject(((ElementMeta)userObject).getName(), userObject);
-		}if(userObject instanceof AttributeMeta){
-			return new MyTreeObject(((AttributeMeta)userObject).getName(), userObject);
-		}
-		
-		return null;
-	}
-
-	/**
-	 * Refresh subtree whose root is the given treeNode reflecting the given object.
-	 * After the refreshing, the given tree will be notified for the update information.
-	 * @param targetNode
-	 * @param object
-	 * @param tree if null, no corresponding tree update information will be broadcast.
-	 */
-	public static void refreshSubTreeByNewMetaObject(DefaultMutableTreeNode targetNode, DefaultMutableTreeNode newNode, JTree tree) 
-	{
-		if(newNode!=null)
-		{//clear out all children and add in new ones, if any
-			targetNode.removeAllChildren();
-			int childCount = newNode.getChildCount();
-			targetNode.setAllowsChildren(true);
-			for(int i=0; i<childCount; i++)
-			{//once a node is moved from newNode to targetNode, it 
-				//is removed from the newNode children list, so always getChildAt(0)
-				targetNode.add((MutableTreeNode) newNode.getChildAt(0));
-			}
-		}
-		targetNode.setUserObject(newNode.getUserObject());
-		if(tree!=null)
-		{
-			TreeModel treeModel = tree.getModel();
-			if (treeModel instanceof DefaultTreeModel)
-			{//notify change.
-				((DefaultTreeModel) treeModel).nodeStructureChanged(targetNode);
-			}
-		}
+		this.nodeMode = mode;
 	}
 
 	/**
@@ -176,7 +57,7 @@ public class ElementMetaLoader implements Serializable
 	 */
 	public TreeNode loadData(Object o)
 	{
-		this.obj = o;
+		this.nodeRoot = o;
 		if (o instanceof ElementMeta){
 			return processElement((ElementMeta)o);
 		}else if(o instanceof Component){
@@ -195,18 +76,18 @@ public class ElementMetaLoader implements Serializable
 	 */
 	private DefaultMutableTreeNode processElement(ElementMeta s)
 	{
-		DefaultMutableTreeNode node = constructTreeNode(s);
-
+		DefaultMutableTreeNode node = constructTreeNode(s, true);
+	
 		List<ElementMeta> childs = s.getChildElement();
 		List<AttributeMeta> fields = s.getAttrData();
 		//Collections.sort(fields, new CSVFieldMetaColumnNumberComparator());
-
+	
 		for (int i = 0; i < fields.size(); i++)
 		{
 			AttributeMeta fieldMeta = fields.get(i);
 			node.add(constructTreeNode(fieldMeta, false));
 		}
-
+	
 		for (int i = 0; i < childs.size(); i++)
 		{
 			ElementMeta childMeta = childs.get(i);
@@ -216,6 +97,63 @@ public class ElementMetaLoader implements Serializable
 		return node;
 	}
 
+	/**
+	 * Construct a tree node
+	 *
+	 * @param userObject
+	 * @param allowsChildren
+	 * @return a tree node that wraps the user object.
+	 */
+	private DefaultMutableTreeNode constructTreeNode(Object userObject, boolean allowsChildren)
+	{
+		DefaultMutableTreeNode node = null;
+		switch(nodeMode){
+		case SOURCE_MODE:
+			node = new DefaultSourceTreeNode(new MyTreeObject(((BaseMeta)userObject).getName(),userObject), allowsChildren);
+			break;
+		case TARGET_MODE:
+			node = new DefaultTargetTreeNode(new MyTreeObject(((BaseMeta)userObject).getName(),userObject), allowsChildren);
+			break;
+		default:
+			node = new DefaultMappableTreeNode(new MyTreeObject(((BaseMeta)userObject).getName(),userObject), allowsChildren);
+		}
+		return node;
+	}
+
+	public class MyTreeObject implements Serializable {
+		private String name;
+		private Object userObject;
+		private Object rootObject;
+		
+		public MyTreeObject(String name, Object obj) {
+			this.rootObject = ElementMetaLoader.this.nodeRoot;
+			this.name = name;
+			this.userObject = obj;
+		}
+		/**
+		 * @return the name
+		 */
+		public String getName() {
+			return name;
+		}
+
+		/**
+		 * @return the obj
+		 */
+		public Object getUserObject() {
+			return userObject;
+		}
+		/**
+		 * @return the rootObj
+		 */
+		public Object getRootObject() {
+			return rootObject;
+		}
+		
+		public String toString(){
+			return name;
+		}
+	}
 }
 /**
  * HISTORY: $Log: not supported by cvs2svn $
