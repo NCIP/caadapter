5943048edd2f67e95e514b64ecd345bd97ab0254
Eugene Wang <wangeug@mail.nih.gov>
2010-03-23 18:21:57 +0000

    clean code, work on recursive issues
    SVN-Revision: 2589

--- src/gov/nih/nci/cbiit/cmts/ui/tree/TreeSelectionHandler.java
+++ src/gov/nih/nci/cbiit/cmts/ui/tree/TreeSelectionHandler.java
@@ -49,7 +49,7 @@ public class TreeSelectionHandler implements TreeSelectionListener {
 			}
 			Object newSelection = treeNode.getUserObject();
 			if(newSelection instanceof ElementMetaLoader.MyTreeObject)
-				newSelection = ((ElementMetaLoader.MyTreeObject)newSelection).getObj();		
+				newSelection = ((ElementMetaLoader.MyTreeObject)newSelection).getUserObject();		
 			graphController.getPropertiesSwitchController().setSelectedItem(newSelection);
 		}
 		graphController.getPropertiesSwitchController().getPropertiesPage().updateProptiesDisplay(null);
