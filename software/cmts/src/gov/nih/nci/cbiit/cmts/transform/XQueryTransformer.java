a60896df3dcab8cbb85961b1833d3f0cece9cc65
Eugene Wang <wangeug@mail.nih.gov>
2010-08-02 19:09:12 +0000

    Use transformer factory
    SVN-Revision: 2786

--- software/cmts/src/gov/nih/nci/cbiit/cmts/transform/XQueryTransformer.java
+++ software/cmts/src/gov/nih/nci/cbiit/cmts/transform/XQueryTransformer.java
@@ -7,6 +7,12 @@
  */
 package gov.nih.nci.cbiit.cmts.transform;
 
+import gov.nih.nci.caadapter.common.ApplicationException;
+import gov.nih.nci.cbiit.cmts.core.Mapping;
+import gov.nih.nci.cbiit.cmts.mapping.MappingFactory;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.Properties;
 
 import javax.xml.xquery.XQConnection;
@@ -15,6 +21,7 @@ import javax.xml.xquery.XQItemType;
 import javax.xml.xquery.XQPreparedExpression;
 import javax.xml.xquery.XQResultSequence;
 
+import javax.xml.bind.JAXBException;
 import javax.xml.namespace.QName;
 
 import net.sf.saxon.Configuration;
@@ -22,52 +29,40 @@ import net.sf.saxon.xqj.SaxonXQDataSource;
 
 /**
  * This class performs the transformation using XQuery
- *
+ * 
  * @author Chunqing Lin
  * @author LAST UPDATE $Author: linc $
- * @since     CMTS v1.0
- * @version    $Revision: 1.3 $
- * @date       $Date: 2008-10-22 19:01:17 $
- *
+ * @since CMTS v1.0
+ * @version $Revision: 1.3 $
+ * @date $Date: 2008-10-22 19:01:17 $
+ * 
  */
-public class XQueryTransformer {
-	// Filename for XML document to query
-	private String sourceFileName;
-	
+public class XQueryTransformer implements TransformationService {
+
 	// Data Source for querying
-//	private SaxonXQDataSource dataSource;
+	// private SaxonXQDataSource dataSource;
 
 	// Connection for querying
 	private XQConnection conn;
+	private boolean temporaryFileCreated = false;
 
-	// Query String
-	private String queryString;
-//	
-//	// Prepared Query
-//	private XQPreparedExpression exp;
-	
+	public boolean isTemporaryFileCreated() {
+		return temporaryFileCreated;
+	}
+
+	public void setTemporaryFileCreated(boolean temporaryFileCreated) {
+		this.temporaryFileCreated = temporaryFileCreated;
+	}
+
 	/**
 	 * constructor
+	 * 
 	 * @throws XQException
 	 */
 	public XQueryTransformer() throws XQException {
-		Configuration  saxonConfig=new Configuration();
+		Configuration saxonConfig = new Configuration();
 		SaxonXQDataSource dataSource = new SaxonXQDataSource(saxonConfig);
 		conn = dataSource.getConnection();
-	}
-
-	/**
-	 * @return the filename
-	 */
-	public final String getFilename() {
-		return sourceFileName;
-	}
-
-	/**
-	 * @param filename the filename to set
-	 */
-	public final void setSourceFileName(String filename) {
-		this.sourceFileName = filename;
 	}
 
 	/**
@@ -77,82 +72,81 @@ public class XQueryTransformer {
 		return conn;
 	}
 
-	/**
-	 * set XQuery
-	 * @param queryString
-	 * @throws XQException
-	 */
-	public void setQuery(String query) throws XQException {
-		queryString=query;
-	}
-	
-	/**
-	 * execute XQuery with pre-set query and source file
-	 * @see #executeQuery(String, String)
-	 * @return
-	 * @throws XQException
-	 */
-	public String executeQuery() throws XQException {
-		return TransformationUtil.formatXqueryResult(executeQuery(queryString, sourceFileName));
-	}
 
 	/**
 	 * execute XQuery
-	 * @param query query string to execute
-	 * @param sourceFile name of source file
+	 * 
+	 * @param query
+	 *            query string to execute
+	 * @param sourceFile
+	 *            name of source file
 	 * @return
 	 * @throws XQException
 	 */
-	public String executeQuery(String query, String sourceFile) throws XQException {
-		XQPreparedExpression exp=conn.prepareExpression(query);
-		exp.bindString(new QName("docName"), sourceFile,
-				conn.createAtomicType(XQItemType.XQBASETYPE_STRING));
+	public String executeQuery(String query, String sourceFile)
+			throws XQException {
+		XQPreparedExpression exp = getConn().prepareExpression(query);
+		exp.bindString(new QName("docName"), sourceFile, conn
+				.createAtomicType(XQItemType.XQBASETYPE_STRING));
 		XQResultSequence result = exp.executeQuery();
-		return result.getSequenceAsString(new Properties());
+		String rawResult = result.getSequenceAsString(new Properties());
+		return TransformationUtil.formatXqueryResult(rawResult);
 	}
-	
-	/**
-	 * test method
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		if (args.length != 1) {
-			System.err.println("Usage: java ibm.dw.xqj.XQueryTester [XML filename]");
-			System.exit(-1);
-		}
 
+	@Override
+	public String Transfer(String sourceFile, String mappingFile) {
+		// TODO Auto-generated method stub
 		try {
-			String xmlFilename = args[0];
-			XQueryTransformer tester= new XQueryTransformer();
-			tester.setSourceFileName(xmlFilename);
-
-			final String sep = System.getProperty("line.separator");
-			String queryString =
-				"declare variable $docName as xs:string external;" + sep +
-				"      for $cd in doc($docName)/CATALOG/CD " +
-				"    where $cd/YEAR > 1980 " +
-				"      and $cd/COUNTRY = 'USA' " +
-				" order by $cd/YEAR " +
-				"   return " +
-				"<cd><title>{$cd/TITLE/text()}</title>" +
-				" <year>{$cd/YEAR/text()}</year></cd>";
-			tester.setQuery(queryString);
-			System.out.println(tester.executeQuery());
-		} catch (Exception e) {
-			e.printStackTrace(System.err);
-			System.err.println(e.getMessage());
+			Mapping map = MappingFactory.loadMapping(new File(mappingFile));
+			XQueryBuilder builder = new XQueryBuilder(map);
+			String queryString = builder.getXQuery();
+			// parse csv data to a temporary file
+			String tempXmlSrc = parseRawData(sourceFile, map);
+			String xmlResult = executeQuery(queryString, tempXmlSrc);
+			if (isTemporaryFileCreated()) {
+				File tmpFile = new File(tempXmlSrc);
+				tmpFile.delete();
+			}
+			return xmlResult;
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (XQException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ApplicationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+
+		return null;
 	}
 
+	/**
+	 * Pre-process raw source data for transformer
+	 * 
+	 * @param sourceRawDataFile
+	 *            URI of source raw data file
+	 * @param map
+	 *            Mapping object
+	 * @return URI of pre-processed source data, it may be a temporary file
+	 * @throws JAXBException
+	 * @throws IOException
+	 * @throws ApplicationException
+	 */
+	protected String parseRawData(String sourceRawDataFile, Mapping map)
+			throws JAXBException, IOException, ApplicationException {
+		// do nothing
+		return sourceRawDataFile;
+	}
 }
 
 /**
- * HISTORY: $Log: not supported by cvs2svn $
- * HISTORY: Revision 1.2  2008/10/20 20:46:15  linc
- * HISTORY: updated.
- * HISTORY:
- * HISTORY: Revision 1.1  2008/10/01 18:59:13  linc
- * HISTORY: updated.
- * HISTORY:
+ * HISTORY: $Log: not supported by cvs2svn $ HISTORY: Revision 1.2 2008/10/20
+ * 20:46:15 linc HISTORY: updated. HISTORY: HISTORY: Revision 1.1 2008/10/01
+ * 18:59:13 linc HISTORY: updated. HISTORY:
  */
 
