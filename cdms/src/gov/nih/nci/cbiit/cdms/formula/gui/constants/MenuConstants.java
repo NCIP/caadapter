b241969a13e86e89102283b175a3efe6efc682b4
Ki Sung Um <umkis@mail.nih.gov>
2010-11-15 21:59:47 +0000

    Formula generator gui constants
    SVN-Revision: 2948

---
+++ cdms/src/gov/nih/nci/cbiit/cdms/formula/gui/constants/MenuConstants.java
@@ -0,0 +1,26 @@
+package gov.nih.nci.cbiit.cdms.formula.gui.constants;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: umkis
+ * Date: Nov 15, 2010
+ * Time: 10:10:59 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface MenuConstants
+{
+	// the menu names.
+	String FILE_MENU_NAME = "File";
+	String NEW_MENU_NAME = "New";
+	String OPEN_MENU_NAME = "Open";
+	String REPORT_MENU_NAME = "Report";
+	String HELP_MENU_NAME = "Help";
+	String TOOLBAR_MENU_NAME = "ToolBar";
+	//Module Name
+	String DB_TO_SDTM="DB_TO_SDTM";
+	String DB_TO_OBJECT="DB_TO_OBJECT";
+	String CSV_TO_XMI="CSV_TO_XMI";
+    String XSD_TO_XMI="XSD_TO_XMI";
+
+}
+
