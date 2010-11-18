beda9c75f8d2cff8b0cb611de7131e1847c48648
Eugene Wang <wangeug@mail.nih.gov>
2010-11-18 15:11:17 +0000

    Use FrameMain singleton
    SVN-Revision: 2962

--- software/cdms/src/gov/nih/nci/cbiit/cdms/formula/gui/action/ExecuteFormulaAction.java
+++ software/cdms/src/gov/nih/nci/cbiit/cdms/formula/gui/action/ExecuteFormulaAction.java
@@ -1,12 +1,10 @@
 package gov.nih.nci.cbiit.cdms.formula.gui.action;
 
 import gov.nih.nci.cbiit.cdms.formula.gui.ExecuteFormulaDialog;
-import gov.nih.nci.cbiit.cdms.formula.gui.FormulaGuiUtil;
+import gov.nih.nci.cbiit.cdms.formula.gui.FrameMain;
 
 import java.awt.event.ActionEvent;
-
 import javax.swing.AbstractAction;
-import javax.swing.JMenuItem;
 
 public class ExecuteFormulaAction extends AbstractAction {
 
@@ -18,8 +16,7 @@ public class ExecuteFormulaAction extends AbstractAction {
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-       JMenuItem menuItem=(JMenuItem)arg0.getSource();
-       ExecuteFormulaDialog wizard = new ExecuteFormulaDialog(FormulaGuiUtil.getMainFrame(), "Execute Formula", true);
+       ExecuteFormulaDialog wizard = new ExecuteFormulaDialog(FrameMain.getSingletonInstance(), "Execute Formula", true);
        wizard.setVisible(true);
       
 	}
