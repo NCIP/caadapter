e717c5011469571d36865c93bb6a6f16594f88dd
Eugene Wang <wangeug@mail.nih.gov>
2010-12-03 21:09:25 +0000

    add/edit parameters
    SVN-Revision: 3019

--- software/cdms/src/gov/nih/nci/cbiit/cdms/formula/gui/action/ExecuteFormulaAction.java
+++ software/cdms/src/gov/nih/nci/cbiit/cdms/formula/gui/action/ExecuteFormulaAction.java
@@ -1,5 +1,6 @@
 package gov.nih.nci.cbiit.cdms.formula.gui.action;
 
+import gov.nih.nci.cbiit.cdms.formula.gui.EditFormulaParameterDialog;
 import gov.nih.nci.cbiit.cdms.formula.gui.ExecuteFormulaDialog;
 import gov.nih.nci.cbiit.cdms.formula.gui.FrameMain;
 
@@ -8,16 +9,28 @@ import javax.swing.AbstractAction;
 
 public class ExecuteFormulaAction extends AbstractAction {
 
-	public ExecuteFormulaAction (String name)
+	public final static int FORMULA_ACTION_ADD_PARAMETER=1;
+	public final static int FORMULA_ACTION_EXECUTION=0;
+	private int type=0;
+	public ExecuteFormulaAction (String name, int dialogType)
 	{
 		super(name);
+		type=dialogType;
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-       ExecuteFormulaDialog wizard = new ExecuteFormulaDialog(FrameMain.getSingletonInstance(), "Execute Formula", true);
-       wizard.setVisible(true);
+		if (type==FORMULA_ACTION_EXECUTION)
+		{
+			ExecuteFormulaDialog wizard = new ExecuteFormulaDialog(FrameMain.getSingletonInstance(), "Execute Formula", true);
+			wizard.setVisible(true);
+		}
+		else if (type==FORMULA_ACTION_ADD_PARAMETER)
+		{
+			EditFormulaParameterDialog wizard = new EditFormulaParameterDialog(FrameMain.getSingletonInstance(), "Add Formula Parameter", true);
+			wizard.setVisible(true);
+		}
       
 	}
 
