47ecb1c7dfdda52455e2e1a8c4ed94af699f054f
Ki Sung Um <umkis@mail.nih.gov>
2010-12-07 06:10:38 +0000

    add webservice
    SVN-Revision: 3029

---
+++ cdms/webservice/src/gov/nih/nci/cbiit/cdms/formula/ws/FormulaCalculatingService.java
@@ -0,0 +1,34 @@
+package gov.nih.nci.cbiit.cdms.formula.ws;
+
+import gov.nih.nci.cbiit.cdms.formula.core.FormulaMeta;
+import gov.nih.nci.cbiit.cdms.formula.FormulaFactory;
+
+import javax.xml.bind.JAXBException;
+import java.util.HashMap;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: umkis
+ * Date: Dec 2, 2010
+ * Time: 10:15:07 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FormulaCalculatingService
+{
+    public String executeCalculate(String formula, HashMap<String, String> paramHash)
+    {
+        FormulaMeta myFormula = null;
+        try
+        {
+            myFormula= FormulaFactory.loadFormula(formula);
+        }
+        catch(JAXBException je)
+        {
+            return je.getMessage();
+        }
+
+        if (paramHash == null) return "no variable";
+
+        return myFormula.getExpression().excute(paramHash);
+    }
+}
