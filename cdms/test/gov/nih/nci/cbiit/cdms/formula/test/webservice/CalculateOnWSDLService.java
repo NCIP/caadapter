614f069263ea3d0a425f68d457cec2a04f1bc473
Ki Sung Um <umkis@mail.nih.gov>
2010-12-07 06:28:15 +0000

    add webservice test
    SVN-Revision: 3034

---
+++ cdms/test/gov/nih/nci/cbiit/cdms/formula/test/webservice/CalculateOnWSDLService.java
@@ -0,0 +1,17 @@
+package gov.nih.nci.cbiit.cdms.formula.test.webservice;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: umkis
+ * Date: Dec 6, 2010
+ * Time: 11:15:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface CalculateOnWSDLService extends javax.xml.rpc.Service {
+    public java.lang.String getCalculateOnWSDLAddress();
+
+    public gov.nih.nci.cbiit.cdms.formula.test.webservice.CalculateOnWSDL getCalculateOnWSDL() throws javax.xml.rpc.ServiceException;
+
+    public gov.nih.nci.cbiit.cdms.formula.test.webservice.CalculateOnWSDL getCalculateOnWSDL(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
+
