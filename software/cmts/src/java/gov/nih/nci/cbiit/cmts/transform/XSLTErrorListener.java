36bf1e2e3910766ba81f19ff24d8da8ddcc43565
Eugene Wang <wangeug@mail.nih.gov>
2011-06-21 13:56:25 +0000

    add XSLT transformer
    SVN-Revision: 3167

---
+++ software/cmts/src/java/gov/nih/nci/cbiit/cmts/transform/XSLTErrorListener.java
@@ -0,0 +1,37 @@
+package gov.nih.nci.cbiit.cmts.transform;
+
+import javax.xml.transform.ErrorListener;
+import javax.xml.transform.TransformerException;
+
+public class XSLTErrorListener implements ErrorListener {
+
+	@Override
+	public void error(TransformerException e) throws TransformerException 
+	{
+		show("Error",e);
+		throw(e);
+
+	}
+
+	@Override
+	public void fatalError(TransformerException e)
+			throws TransformerException 
+	{
+		show("Fatal ",e);
+		throw(e);
+	}
+
+	@Override
+	public void warning(TransformerException e) throws TransformerException 
+	{
+		show("Warning",e);
+		throw(e);
+	}
+	
+	private void show(String type,TransformerException e) 
+	{
+		System.out.println(type + ": " + e.getMessage());
+		if(e.getLocationAsString() != null)
+			System.out.println(e.getLocationAsString());
+	}
+}
