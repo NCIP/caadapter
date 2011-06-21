0801c3dbb2819ef8a6ddbd1704bfa293c48ce65b
Eugene Wang <wangeug@mail.nih.gov>
2011-06-21 13:56:25 +0000

    add XSLT transformer
    SVN-Revision: 3167

---
+++ src/java/gov/nih/nci/cbiit/cmts/transform/XsltTransformer.java
@@ -0,0 +1,23 @@
+package gov.nih.nci.cbiit.cmts.transform;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+public class XsltTransformer {
+
+	public void transform(String inXML,String inXSL,String outTXT)
+	  throws TransformerConfigurationException, TransformerException 
+	  {
+		 javax.xml.transform.TransformerFactory xmlTransFactory = javax.xml.transform.TransformerFactory.newInstance();
+		 StreamSource xslStream = new StreamSource(inXSL);
+		 Transformer transformer = xmlTransFactory.newTransformer(xslStream);
+		 transformer.setErrorListener(new XSLTErrorListener());
+	
+		 StreamSource in = new StreamSource(inXML);
+		 StreamResult out = new StreamResult(outTXT);
+		 transformer.transform(in,out);
+	  }
+}
