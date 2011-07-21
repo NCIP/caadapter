862575bda82a3e18d27acf30341e5e5b58a80b0e
Eugene Wang <wangeug@mail.nih.gov>
2011-07-21 19:14:53 +0000

    generate indent xml file output
    SVN-Revision: 3211

--- src/java/gov/nih/nci/cbiit/cmts/transform/XsltTransformer.java
+++ src/java/gov/nih/nci/cbiit/cmts/transform/XsltTransformer.java
@@ -1,5 +1,6 @@
 package gov.nih.nci.cbiit.cmts.transform;
 
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
@@ -15,7 +16,8 @@ public class XsltTransformer {
 		 StreamSource xslStream = new StreamSource(inXSL);
 		 Transformer transformer = xmlTransFactory.newTransformer(xslStream);
 		 transformer.setErrorListener(new XSLTErrorListener());
-	
+		 transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
 		 StreamSource in = new StreamSource(inXML);
 		 StreamResult out = new StreamResult(outTXT);
 		 transformer.transform(in,out);
