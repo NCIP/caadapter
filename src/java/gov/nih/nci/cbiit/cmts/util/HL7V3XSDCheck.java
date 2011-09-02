9aedccd7f62c459d3f4ccdae3ae0806d848266b3
Eugene Wang <wangeug@mail.nih.gov>
2011-09-02 17:47:46 +0000

    verify HL7v3 schema
    SVN-Revision: 3250

---
+++ src/java/gov/nih/nci/cbiit/cmts/util/HL7V3XSDCheck.java
@@ -0,0 +1,23 @@
+package gov.nih.nci.cbiit.cmts.util;
+
+import gov.nih.nci.cbiit.cmts.common.XSDParser;
+import gov.nih.nci.cbiit.cmts.core.BaseMeta;
+import gov.nih.nci.cbiit.cmts.core.ElementMeta;
+
+public class HL7V3XSDCheck {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		XSDParser p = new XSDParser();
+		String dirPath=args[0];
+		String xsdFile=args[1];//"FICR_IN926306UV03.xsd"; //args[0];
+		String elmName=xsdFile.substring(0,xsdFile.indexOf(".xsd"));
+		p.loadSchema(dirPath+"/multicacheschemas/"+xsdFile, null);
+		ElementMeta e = p.getElementMeta("urn:hl7-org:v3", elmName);	
+		System.out.println("HL7V3XSDCheck.main()...metacount:"+BaseMeta.metaCount);
+	}
+
+}
