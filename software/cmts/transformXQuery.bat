143c41f33b84995b4278c11d57e3f19cce6a944f
Eugene Wang <wangeug@mail.nih.gov>
2011-09-30 17:15:18 +0000

    use "instruction" for transformation mapping
    SVN-Revision: 3278

--- software/cmts/transformXQuery.bat
+++ software/cmts/transformXQuery.bat
@@ -1,6 +1,6 @@
 set source=workingspace\simpleMapping\shiporder.xml 
-set xquery=workingspace\simpleMapping\testXQ.xq
+set instruction=workingspace\simpleMapping\testXQ.xq
 set result=workingspace\simpleMapping\xqueryOut.xml
 set libpath=lib\saxon9.jar;lib\saxon9-xqj.jar;lib\jdom.jar;dist\cmts.jar;
 
-java -cp %libpath% gov.nih.nci.cbiit.cmts.transform.XQueryTransformer %source% %xquery% %result%
+java -cp %libpath% gov.nih.nci.cbiit.cmts.transform.XQueryTransformer %source% %instruction% %result%
