9927f523988b7dd20d76a231f19dc1735c5ef3c2
Ki Sung Um <umkis@mail.nih.gov>
2011-10-27 15:58:18 +0000

    SVN-Revision: 3295

---
+++ software/dvts/src/java/gov/nih/nci/caadapter/dvts/common/meta/MappingResults.java
@@ -0,0 +1,70 @@
+package gov.nih.nci.caadapter.dvts.common.meta;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for mappingResults complex type.
+ *
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ *
+ * <pre>
+ * &lt;complexType name="mappingResults">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="Result" type="{http://www.w3.org/2001/XMLSchema}string" maxOccurs="unbounded"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ *
+ *
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "mappingResults", propOrder = {
+    "result"
+})
+public class MappingResults {
+
+    @XmlElement(name = "Result", required = true)
+    protected List<String> result;
+
+    /**
+     * Gets the value of the result property.
+     *
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the result property.
+     *
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getResult().add(newItem);
+     * </pre>
+     *
+     *
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link String }
+     *
+     *
+     */
+    public List<String> getResult() {
+        if (result == null) {
+            result = new ArrayList<String>();
+        }
+        return this.result;
+    }
+
+}
