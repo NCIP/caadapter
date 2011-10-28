5f74067dd06d28f998df7d4eafde43ddfb883063
Eugene Wang <wangeug@mail.nih.gov>
2011-10-28 19:26:04 +0000

    modify
    SVN-Revision: 3313

--- cmts/test/webServiceClient/runWsClient.bat
+++ cmts/test/webServiceClient/runWsClient.bat
@@ -1,15 +1,28 @@
 # QA server
+
 # SET END_URL=http://cbioqa101.nci.nih.gov:49080/caAdapterWS/ws/caAdapterTransformationService
+
 # caAdpater Development server
+
 # SET END_URL=http://cbiovdev5054.nci.nih.gov/caAdapterWS/ws/caAdapterTransformationService
+
 # local server
 SET END_URL=http://localhost:30210/caadapterWS-cmts/services/cmtsTransformationService
-#SET END_URL=http://localhost:30210/caadapterWS-cmts/ws/AxisServlet/cmtsTransformationService
-# scenarioName
-SET SCENARIO_NAME=testThree
 
-# sourceFileName, 
+# SET END_URL=http://localhost:30210/caadapterWS-cmts/ws/AxisServlet/cmtsTransformationService
+
+# scenarioName
+
+SET SCENARIO_NAME=testOne
+
+
+# sourceFileName 
+
 SET SRC_FILE=shiporder.xml
+
 SET SRC_TYPE=xml
+
+
+#java -cp cmtsWsClient.jar;lib/axis.jar;lib/jaxrpc.jar;lib/commons-logging-1.0.4.jar;lib/commons-discovery-0.2.jar;lib/saaj.jar;lib/wsdl4j-1.5.1.jar gov.nih.nci.cbiit.cmts.test.ws.client.CmtsWebserviceClient %SCENARIO_NAME% %SRC_FILE% %SRC_TYPE% %2 %END_URL%
 
 java -cp cmtsWsClient.jar;lib/axis.jar;lib/jaxrpc.jar;lib/commons-logging-1.0.4.jar;lib/commons-discovery-0.2.jar;lib/saaj.jar;lib/wsdl4j-1.5.1.jar gov.nih.nci.cbiit.cmts.test.ws.client.CmtsWebserviceClient %SCENARIO_NAME% %SRC_FILE% %SRC_TYPE% %2 %END_URL%
