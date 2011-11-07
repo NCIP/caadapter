c1383100768c042aaf7b47aeb8070fa94d31d13b
Eugene Wang <wangeug@mail.nih.gov>
2011-11-07 20:45:04 +0000

    Migrate to CXF 2.4.2
    SVN-Revision: 3333

---
+++ cmts/src/webservices/src/gov/nih/nci/cbiit/cmts/ws/ResultList.java
@@ -0,0 +1,21 @@
+package gov.nih.nci.cbiit.cmts.ws;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+ 
+@XmlRootElement(name = "ReulstData")
+public class ResultList {
+ 
+    @XmlElement(name = "result", required = true)
+    private List <String> result;
+ 
+    public List<String> getResultData() {
+        if (result == null) {
+        	result = new ArrayList<String>();
+        }
+        return this.result;
+    }
+}
