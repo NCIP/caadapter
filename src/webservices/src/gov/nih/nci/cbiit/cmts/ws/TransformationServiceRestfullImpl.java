df869e7547e3c2185f9cfbf962ea65cbdc09cb9d
Eugene Wang <wangeug@mail.nih.gov>
2011-11-07 20:45:04 +0000

    Migrate to CXF 2.4.2
    SVN-Revision: 3333

---
+++ src/webservices/src/gov/nih/nci/cbiit/cmts/ws/TransformationServiceRestfullImpl.java
@@ -0,0 +1,23 @@
+package gov.nih.nci.cbiit.cmts.ws;
+
+public class TransformationServiceRestfullImpl implements TransformationWebServiceRestfull{
+
+	private TransformationWebService wsServer;
+	@Override
+	public ResultList restfullService(String mappingScenario,
+			String sourceData) {
+//		if (wsServer==null)
+//			wsServer=new TransformationServiceImpl();
+		System.out
+				.println("TransformationServiceRestfullImpl.restfullService()...called");
+		System.out
+				.println("TransformationServiceRestfullImpl.restfullService()...scenario:"+mappingScenario);
+		System.out
+				.println("TransformationServiceRestfullImpl.restfullService()...sourcedata:\n"+sourceData);
+//		ArrayList<String> rtnList=wsServer.transformationService(mappingScenario, sourceData);
+		ResultList rtnList= new ResultList();
+		rtnList.getResultData().add("mappingScenario="+mappingScenario);
+		rtnList.getResultData().add("...sourceData="+sourceData);
+		return rtnList;
+	}	
+}
