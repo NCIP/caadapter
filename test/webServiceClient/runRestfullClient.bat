b14e4c5f939033c9ebe985f725b3b44360ef87d9
Eugene Wang <wangeug@mail.nih.gov>
2011-11-09 15:59:47 +0000

    support RESTFull client
    SVN-Revision: 3340

---
+++ test/webServiceClient/runRestfullClient.bat
@@ -0,0 +1,32 @@
+# QA server
+
+# SET END_URL=http://cbioqa101.nci.nih.gov:49080/caAdapterWS/services/restfull/transfer
+
+# caAdpater Development server
+
+# SET END_URL=http://cbiovdev5054.nci.nih.gov/caAdapterWS/services/restfull/transfer
+
+# local server
+SET 
+# END_URL=http://localhost:30210/caadapterWS-cmts/services/restfull/transfer
+
+# SET END_URL=http://localhost:8080/caadapterWS-cmts/services/restfull/transfer
+
+# DEV server
+SET END_URL=http://caadapter-dev.nci.nih.gov/caadapterWS-cmts/services/restfull/transfer
+
+# scenarioName
+
+SET SCENARIO_NAME=testTwo
+
+
+# sourceFileName 
+
+SET SRC_FILE=shipOrder.xml
+
+# SET SRC_TYPE=xml
+
+
+
+
+java -cp wsClient.jar;lib\cxf-2.4.2.jar gov.nih.nci.cbiit.cmts.test.restfull.Client %SCENARIO_NAME% %SRC_FILE% %2 %END_URL%
