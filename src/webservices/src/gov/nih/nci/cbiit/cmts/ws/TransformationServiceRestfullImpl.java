4aee31feaef5774bda1a6061fa6de28cebaec179
Eugene Wang <wangeug@mail.nih.gov>
2011-11-09 14:46:40 +0000

    support RESTfull client
    SVN-Revision: 3339

--- src/webservices/src/gov/nih/nci/cbiit/cmts/ws/TransformationServiceRestfullImpl.java
+++ src/webservices/src/gov/nih/nci/cbiit/cmts/ws/TransformationServiceRestfullImpl.java
@@ -1,23 +1,30 @@
 package gov.nih.nci.cbiit.cmts.ws;
 
+import java.util.ArrayList;
+
 public class TransformationServiceRestfullImpl implements TransformationWebServiceRestfull{
 
 	private TransformationWebService wsServer;
 	@Override
 	public ResultList restfullService(String mappingScenario,
 			String sourceData) {
-//		if (wsServer==null)
-//			wsServer=new TransformationServiceImpl();
+		if (wsServer==null)
+			wsServer=new TransformationServiceImpl();
 		System.out
 				.println("TransformationServiceRestfullImpl.restfullService()...called");
 		System.out
 				.println("TransformationServiceRestfullImpl.restfullService()...scenario:"+mappingScenario);
 		System.out
 				.println("TransformationServiceRestfullImpl.restfullService()...sourcedata:\n"+sourceData);
-//		ArrayList<String> rtnList=wsServer.transformationService(mappingScenario, sourceData);
+		ArrayList<String> transList=wsServer.transformationService(mappingScenario, sourceData);
 		ResultList rtnList= new ResultList();
+
 		rtnList.getResultData().add("mappingScenario="+mappingScenario);
-		rtnList.getResultData().add("...sourceData="+sourceData);
+		rtnList.getResultData().add("sourceData="+sourceData);
+		for (String transResult:transList)
+		{
+			rtnList.getResultData().add(transResult);
+		}
 		return rtnList;
 	}	
 }
