760d05fb38af2ed7539a1587ec2ef59e5c2510eb
Eugene Wang <wangeug@mail.nih.gov>
2011-11-10 14:54:34 +0000

    clean codes:RESTfull with CXF
    SVN-Revision: 3344

--- software/cmts/test/webServiceClient/runRestfullClient.bat
+++ software/cmts/test/webServiceClient/runRestfullClient.bat
@@ -1,19 +1,18 @@
 # QA server
 
-# SET END_URL=http://cbioqa101.nci.nih.gov:49080/caAdapterWS/services/restfull/transfer
+# SET END_URL=http://cbioqa101.nci.nih.gov:49080/caAdapterWS/services/restful/transfer
 
 # caAdpater Development server
 
-# SET END_URL=http://cbiovdev5054.nci.nih.gov/caAdapterWS/services/restfull/transfer
+# SET END_URL=http://cbiovdev5054.nci.nih.gov/caAdapterWS/services/restful/transfer
 
 # local server
-SET 
-# END_URL=http://localhost:30210/caadapterWS-cmts/services/restfull/transfer
+SET END_URL=http://localhost:30210/caadapterWS-cmts/services/restful/transfer
 
-# SET END_URL=http://localhost:8080/caadapterWS-cmts/services/restfull/transfer
+# SET END_URL=http://localhost:8080/caadapterWS-cmts/services/restful/transfer
 
 # DEV server
-SET END_URL=http://caadapter-dev.nci.nih.gov/caadapterWS-cmts/services/restfull/transfer
+# SET END_URL=http://caadapter-dev.nci.nih.gov/caadapterWS-cmts/services/restfull/transfer
 
 # scenarioName
 
