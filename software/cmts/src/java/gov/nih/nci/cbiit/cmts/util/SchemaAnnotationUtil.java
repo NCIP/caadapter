7da723600c48a578f67eb4288846571524677958
Eugene Wang <wangeug@mail.nih.gov>
2012-01-04 17:09:09 +0000

    process annotation
    SVN-Revision: 3432

---
+++ software/cmts/src/java/gov/nih/nci/cbiit/cmts/util/SchemaAnnotationUtil.java
@@ -0,0 +1,33 @@
+package gov.nih.nci.cbiit.cmts.util;
+
+public class SchemaAnnotationUtil {
+
+	public static String extractTagValue(String value, String tagName)
+	{
+		if (value==null||tagName==null)
+			return null;
+		if (value.indexOf(tagName)<0)
+			return null;
+		String tagValue=value.substring(value.indexOf(tagName), value.lastIndexOf(tagName));
+		if (tagValue==null)
+			return null;
+		if (tagValue.indexOf(">")<0)
+			return tagValue;
+		if (tagValue.indexOf("</")<0)
+			return tagValue;
+		String rtnValue=tagValue.substring(tagValue.indexOf(">")+1, tagValue.lastIndexOf("</"));
+		rtnValue=SchemaAnnotationUtil.trim(rtnValue,' ');
+		rtnValue.replaceAll("\\s", "");
+		return rtnValue;
+	}
+	
+	private static String trim(String source, char c) 
+	{      
+		int len = source.length();      
+		int st = 0;      
+		char[] val = source.toCharArray();      
+		for (;(st < len) && (val[st] <= c);st++){}      
+		for (;(st < len) && (val[len-1]<=c);len--){}      
+		return ((st > 0) || (len < source.length())) ?source.substring(st, len) : source;   
+	}
+}
